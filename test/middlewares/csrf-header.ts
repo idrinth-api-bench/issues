cb5ca04dadbfc060cacb9cc5e26874528df3ce45
Björn Büttner <Idrinth@users.noreply.github.com>
2020-07-17 19:13:44 +0200

    testing cookie and csrf-header middlewres

--- test/middlewares/csrf-header.ts
+++ test/middlewares/csrf-header.ts
@@ -3,6 +3,8 @@ import {
   expect,
 } from 'chai';
 import 'mocha';
+import { Request } from '../../src/request';
+import { Result } from '../../src/result';
 
 describe('middlewares/csrf-header', () => {
   it('should be a class', () => {
@@ -13,5 +15,20 @@ describe('middlewares/csrf-header', () => {
   },);
   it('should have a static method process', () => {
     expect(CsrfHeader.process,).to.be.a('function',);
+  },);
+  it('should not set token by default', () => {
+    expect(CsrfHeader.prepare(<Request>{}),).to.deep.equal({headers: {}},);
+  },);
+  it('should get token by default', () => {
+    expect(() => CsrfHeader.process(<Result><unknown>{response: {headers: {'x-csrf-token': 'def'}}}),).to.not.throw();
+  },);
+  it('should set token it has', () => {
+    expect(CsrfHeader.prepare(<Request>{}),).to.deep.equal({headers: {'x-csrf-token': 'def'}},);
+  },);
+  it('should change no token if there\'s none', () => {
+    expect(() => CsrfHeader.process(<Result><unknown>{response: {headers: {}}}),).to.not.throw();
+  },);
+  it('should set token it has', () => {
+    expect(CsrfHeader.prepare(<Request>{}),).to.deep.equal({headers: {'x-csrf-token': 'def'}},);
   },);
 },);
