e460ec9a1dc97720c9c66de6bbf3c7008314bff6
Björn Büttner <Idrinth@users.noreply.github.com>
2020-07-23 15:46:24 +0200

    testing validator

--- test/worker/validator.ts
+++ test/worker/validator.ts
@@ -9,4 +9,43 @@ describe('validator', () => {
   it('should be a function', () => {
     expect(validator,).to.be.a('function',);
   },);
+  it('should not test failures', () => {
+    expect(validator({
+      id: '+',
+      duration: null,
+      msg: 'Döner macht schöner',
+      success: false,
+      validators: [],
+    },),).to.deep.equal({
+      success: false,
+      msg: 'Döner macht schöner',
+      id: '+',
+      duration: null,
+    },);
+  },);
+  it('should fail if validator is missing', () => {
+    const duration = 12;
+    const result = validator({
+      id: '+',
+      duration,
+      validators: [ __dirname + '/a.js', ],
+    },);
+    expect(result.id,).to.equal('+',);
+    expect(result.duration,).to.equal(duration,);
+    //eslint-disable-next-line no-unused-expressions
+    expect(result.success,).to.be.false;
+    expect(result.msg + '',).to.match(/^Error: Cannot find module .*?a.js/u,);
+  },);
+  it('should succeed if validators throw no error', () => {
+    const duration = 92;
+    const result = validator({
+      id: '#',
+      duration,
+      validators: [],
+    },);
+    expect(result.id,).to.equal('#',);
+    expect(result.duration,).to.equal(duration,);
+    //eslint-disable-next-line no-unused-expressions
+    expect(result.success,).to.be.true;
+  },);
 },);
