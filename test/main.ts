8b1e2fe09aa014afe71637e09c85fdd3f05c428d
Björn Büttner <Idrinth@users.noreply.github.com>
2020-12-01 22:33:00 +0100

    adding very simple tests for main calls

--- test/main.ts
+++ test/main.ts
@@ -3,9 +3,22 @@ import {
   expect,
 } from 'chai';
 import 'mocha';
+import Reporter from '../src/reporter/reporter';
+import { NullLogger } from '../src/logger/null-logger';
+
+const NOOPR: Reporter = () => {/* noop */};
 
 describe('main', () => {
   it('should be a function', () => {
     expect(main,).to.be.a('function',);
+  },);
+  it('can be called with 3 params', () => {
+    expect(() => main(1, 1, []),).to.not.throw;
+  },);
+  it('can be called with 4 params', () => {
+    expect(() => main(1, 1, [], NOOPR),).to.not.throw;
+  },);
+  it('can be called with 5 params', () => {
+    expect(() => main(1, 1, [], NOOPR, new NullLogger()),).to.not.throw;
   },);
 },);
