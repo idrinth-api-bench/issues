0c08848d191e4a9c9eb3249e18bfdf4c9b860ce5
Björn Büttner <bjoern.buettner@jungwild.io>
2023-12-10 16:57:20 +0100

    2.2.0 - console logger - fixes to executor - fixes to tests

--- worker/webrequest.js
+++ worker/webrequest.js
@@ -1,5 +1,5 @@
 import Worker from 'worker_threads';
-import worker from '../src/worker/runner';
+import worker from '../src/worker/runner.js';
 
 const reply = (response,) => Worker.parentPort.postMessage(response,);
 
