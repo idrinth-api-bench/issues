bc66cd7e8b5007384d7e0fc00eb1c7605a9952c3
Björn Büttner <Idrinth@users.noreply.github.com>
2023-12-20 01:13:20 +0100

    add simple html reporter

--- src/helper/default-reporter.ts
+++ src/helper/default-reporter.ts
@@ -2,9 +2,11 @@ import multiReporter from '../reporter/multi-reporter.js';
 import csvReporter from '../reporter/csv-reporter.js';
 import consoleReporter from '../reporter/console-reporter.js';
 import jsonReporter from '../reporter/json-reporter.js';
+import htmlReporter from '../reporter/html-reporter.js';
 
 multiReporter.addReporter(csvReporter,);
 multiReporter.addReporter(consoleReporter,);
 multiReporter.addReporter(jsonReporter,);
+multiReporter.addReporter(htmlReporter,);
 
 export default multiReporter;
