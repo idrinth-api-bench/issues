114c2dd604492a906f3246e1ea60d84ba185acab
David <david.henri.santana@gmail.com>
2024-01-01 06:55:21 -0300

    feat: eslint changes

--- website/src/pages/logging/index.tsx
+++ website/src/pages/logging/index.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import {
+  docco,
+} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-const Logging = () => (
-  <>
-    <div className="title-card">
-      <h1>Logging</h1>
-      <p>
+const Logging = () => <>
+  <div className="title-card">
+    <h1>Logging</h1>
+    <p>
         Any logger that either implements the interface or has a wrapper is an
         option. Wrappers for pino and winston are available.
-      </p>
-    </div>
-    <div className="content">
-      <h2>Logging levels used</h2>
-      <p>
+    </p>
+  </div>
+  <div className="content">
+    <h2>Logging levels used</h2>
+    <p>
         Most log entries are debug level, with the major steps being written to
         info. Trace is currently not used but may be used for detailed argument
         printing at some point.
-      </p>
-      <h2>Custom Logger</h2>
-      <p>
+    </p>
+    <h2>Custom Logger</h2>
+    <p>
         You can implement the logger interface below and provide any logger you
         want to next to the already provided ones.
-      </p>
-      <SyntaxHighlighter language="javascript" style={docco}>
-          {`interface Logger {
+    </p>
+    <SyntaxHighlighter language="javascript" style={docco}>
+      {`interface Logger {
   trace(msg: string, data: Record<string, unknown>): void;
   trace(msg: string): void;
   debug(msg: string, data: Record<string, unknown>): void;
@@ -38,15 +39,12 @@ const Logging = () => (
   fatal(msg: string, data: Record<string, unknown>): void;
   fatal(msg: string): void;
 }`}
-</SyntaxHighlighter>
+    </SyntaxHighlighter>
 
-      <p>
+    <p>
         For ease of use, a wrapper is provided as well, that only requires a log
         method to be implemented.
-      </p>
-    </div>
-  </>
-);
-
-
+    </p>
+  </div>
+</>;
 export default Logging;
--- website/src/pages/storage/index.tsx
+++ website/src/pages/storage/index.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import {
+  docco,
+} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-const Storage = () => (
-  <>
-    <div className="title-card">
-      <h1>Storage</h1>
-      <p>
+const Storage = () => <>
+  <div className="title-card">
+    <h1>Storage</h1>
+    <p>
         Storage classes store each finished set of the run. By default, nothing
         is stored, but a mysql storage is provided for those wanting it.
-      </p>
-    </div>
-    <div className="content">
-      <h2>Custom Storage</h2>
-      <p>
+    </p>
+  </div>
+  <div className="content">
+    <h2>Custom Storage</h2>
+    <p>
         To provide a custom storage, implement the interface below. The method
         will be called once per task.
-      </p>
-      <SyntaxHighlighter language="javascript" style={docco}>
-            {`interface Storage
+    </p>
+    <SyntaxHighlighter language="javascript" style={docco}>
+      {`interface Storage
 {
   store(data: FinishedSet, now: Date): void;
 }`}
-      </SyntaxHighlighter>
-    </div>
-  </>
-);
+    </SyntaxHighlighter>
+  </div>
+</>;
 export default Storage;
