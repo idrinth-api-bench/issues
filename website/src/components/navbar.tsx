e6d8517dcc57e1e3bbd3949c101cf3d2cedab738
David <david.henri.santana@gmail.com>
2024-01-02 06:28:06 -0300

    fix: lint and eslint changes

--- website/src/components/navbar.tsx
+++ website/src/components/navbar.tsx
@@ -6,55 +6,52 @@ import {
   NavLink,
 } from 'react-router-dom';
 
-const Navbar = () => (
-  <>
-    <header>
-      <div>
-        <img src={Readme} alt="readme" />
-        <strong>@idrinth/api-bench</strong>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">{"Home"}</NavLink>
-          </li>
-          <li>
-            <a href="https://www.npmjs.com/" target="_blank">
+const Navbar = () => <>
+  <header>
+    <div>
+      <img src={Readme} alt="readme" />
+      <strong>@idrinth/api-bench</strong>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">{'Home'}</NavLink>
+        </li>
+        <li>
+          <a href="https://www.npmjs.com/" target="_blank">
               NPMJS
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/Idrinth/api-bench" target="_blank">
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/Idrinth/api-bench" target="_blank">
               GITHUB
-            </a>
-          </li>
-          <li>
-            <NavLink to="/usage">{"Usage & Examples"}</NavLink>
-            <ul>
-              <li>
-                <NavLink to="/usage/autowiring">{"Autowiring"}</NavLink>
-              </li>
-              <li>
-                <NavLink to="/usage/results">{"Results"}</NavLink>
-              </li>
-              <li>
-                <NavLink to="/usage/logging">{"Logging"}</NavLink>
-              </li>
-              <li>
-                <NavLink to="/usage/middlewares">{"Middlewares"}</NavLink>
-              </li>
-              <li>
-                <NavLink to="/usage/storage">{"Storage"}</NavLink>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <NavLink to="/imprint">{'Imprint'}</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </>
-);
-
+          </a>
+        </li>
+        <li>
+          <NavLink to="/usage">{'Usage & Examples'}</NavLink>
+          <ul>
+            <li>
+              <NavLink to="/usage/autowiring">{'Autowiring'}</NavLink>
+            </li>
+            <li>
+              <NavLink to="/usage/results">{'Results'}</NavLink>
+            </li>
+            <li>
+              <NavLink to="/usage/logging">{'Logging'}</NavLink>
+            </li>
+            <li>
+              <NavLink to="/usage/middlewares">{'Middlewares'}</NavLink>
+            </li>
+            <li>
+              <NavLink to="/usage/storage">{'Storage'}</NavLink>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <NavLink to="/imprint">{'Imprint'}</NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</>;
 export default Navbar;
