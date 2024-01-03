23ccc00fe08dfea20117eff74e92336d81ba770d
Björn Büttner <Idrinth@users.noreply.github.com>
2024-01-03 10:11:23 +0100

    add missing commas

--- website/src/i18n.ts
+++ website/src/i18n.ts
@@ -6,9 +6,9 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
+  .use(Backend,)
+  .use(LanguageDetector,)
+  .use(initReactI18next,)
   .init({
     fallbackLng: 'en',
     interpolation: {
