6d1998884de4c911cae706989fde1a5959574dcb
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-01-04 16:58:32 -0500

    Fixed nav bar design for website

--- website/src/components/footer.tsx
+++ website/src/components/footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => <footer>
-  <span>© 2020-2024 Björn Büttner and contributors.</span>
+  <span className='footer-text'>© 2020-2024 Björn Büttner and contributors.</span>
   <ul id="policy-list">
     <li>
       <a href="#" >About</a>
