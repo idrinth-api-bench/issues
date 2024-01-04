0172379a9ae3618caaee27f6097fe90d2dfea5b2
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-01-03 19:30:24 -0500

    Completed footer styling but no href links

--- website/src/pages/home/index.tsx
+++ website/src/pages/home/index.tsx
@@ -56,28 +56,30 @@ const Index = () => <section className="home">
     </p>
   </div>
   <div>
-    <footer  className='footermainrow'>
-      <div className='div-block'>
-        <a href="#" className='logofooter'>
-          <img className="imglogo" src="/src/assets/readme-svgrepo-com.svg" alt="readme" width={70}/>
-          <strong>@idrinth/api-bench</strong>
-        </a>
-        <p className='footertext'>
-        Efficiently test APIs & sites for response time changes,
-        simplifying code performance detection
-        </p>
-      </div>
-      <div className='footerfromblock'>
-        <p className='footercaps'>
-        OPTIMIZE RESPONSE TIMES. SIMPLIFY CODE PERFORMANCE.
-        </p>
-      </div>
-    </footer>
-    <footer className='footerSecondRow'>
-      <div className='termcondblock'>
-        <p>© 2020 Björn Büttner and contributors. All Rights Reserved</p>
-        <a href="#" className='footerlink'>privacy policy</a>
-        <a href="#" className='footerlink'>Terms and conditions</a>
+  <footer className="bg-[#242329]">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a href="#" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="/src/assets/readme-svgrepo-com.svg" className="h-8" alt="logo" />
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">@idrinth/api-bench</span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2020 <a href="#" className="hover:underline">Björn Büttner and contributors</a>. All Rights Reserved.</span>
       </div>
     </footer>
   </div>
