2c07a23502dea8a8b4bdef6f02eec1961368d42f
Björn Büttner <Idrinth@users.noreply.github.com>
2024-01-06 05:44:18 +0100

    remove unused function

--- website/cypress.config.ts
+++ website/cypress.config.ts
@@ -3,9 +3,5 @@ import {
 } from 'cypress';
 
 export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config,) {
-      // implement node event listeners here
-    },
-  },
+  e2e: {},
 },);
