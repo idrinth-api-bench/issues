7c56a1433a7defe86fb19d5619bdbd6002cec126
Björn Büttner <Idrinth@users.noreply.github.com>
2024-01-09 06:48:27 +0100

    add meta description, add titles fixes #107 fixes #93

--- website/test/components/default-meta.ts
+++ website/test/components/default-meta.ts
@@ -7,11 +7,4 @@ describe('components/default-meta', () => {
   it('should be a function', () => {
     expect(DefaultMeta,).to.be.a('function',);
   },);
-  it('() should be an object', () => {
-    const result = DefaultMeta({
-      page: 'home',
-      path: '',
-    },);
-    expect(result,).to.be.a('object',);
-  },);
 },);
