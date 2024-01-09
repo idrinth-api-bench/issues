9ca273519a543f0b995260cd626f612ea32e8251
Björn Büttner <Idrinth@users.noreply.github.com>
2024-01-09 07:06:29 +0100

    add missing space

--- website/cypress/e2e/results.cy.ts
+++ website/cypress/e2e/results.cy.ts
@@ -1,6 +1,6 @@
 describe('usage/results', () => {
   it('can be visited', () => {
     cy.visit('http://localhost:8080/usage/results',);
-    cy.title().should('eq', 'Results |Usage | @idrinth/api-bench',);
+    cy.title().should('eq', 'Results | Usage | @idrinth/api-bench',);
   },);
 },);
