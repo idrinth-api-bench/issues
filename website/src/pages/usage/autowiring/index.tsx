594db163ad828e0d912fb2ffec6e05fd44d925d0
Björn Büttner <Idrinth@users.noreply.github.com>
2024-01-10 04:44:08 +0100

    fix metadata

--- website/src/pages/usage/autowiring/index.tsx
+++ website/src/pages/usage/autowiring/index.tsx
@@ -2,40 +2,40 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import Layout from '../../../components/layout.tsx';
 
-const AutoWiring = () => <Layout Outlet={<section>
-  <div className='title-card'>
-    <h1>Autowiring Route Parameters</h1>
-    <p>
-        Parameters of contained functions will be filled with environment
-        variable values of the respective name (aBc {'->'} A_BC).
-      <br /> Types will be automatically applied if there is either a default
-        value to get the type from or a comment like /* boolean */ parameter
-        added in front of the parameter.
-      <br /> Comment types will always win over default value types. If those
-        two don't match, you might get weird results. Sadly, a few things can't
-        be done:
-    </p>
-  </div>
-  <div className='card'>
-    <h2>Limitations</h2>
-    <ul>
-      <li>
-          Do not use constants for their default values, they can't be properly
-          analyzed and will throw an Error.
-      </li>
-      <li>
-          Do not use arrays or objects as default values, they can't be
-          autowired.
-      </li>
-      <li>Destructuring is not supported</li>
-      <li>Default value types NOT of the same type as the parameter</li>
-    </ul>
-  </div>
-  <div className='card'>
-    <h2>Example</h2>
-    <SyntaxHighlighter language='javascript'>
-
-      {`module.exports = (apiRootUrl, apiEMail, apiPassword) => ({
+const AutoWiring = () => <Layout
+  Outlet={<section>
+    <div className='title-card'>
+      <h1>Autowiring Route Parameters</h1>
+      <p>
+          Parameters of contained functions will be filled with environment
+          variable values of the respective name (aBc {'->'} A_BC).
+        <br /> Types will be automatically applied if there is either a default
+          value to get the type from or a comment like /* boolean */ parameter
+          added in front of the parameter.
+        <br /> Comment types will always win over default value types. If those
+          two don't match, you might get weird results. Sadly, a few things
+          can't be done:
+      </p>
+    </div>
+    <div className='card'>
+      <h2>Limitations</h2>
+      <ul>
+        <li>
+            Do not use constants for their default values, they can't be
+            properly analyzed and will throw an Error.
+        </li>
+        <li>
+            Do not use arrays or objects as default values, they can't be
+            autowired.
+        </li>
+        <li>Destructuring is not supported</li>
+        <li>Default value types NOT of the same type as the parameter</li>
+      </ul>
+    </div>
+    <div className='card'>
+      <h2>Example</h2>
+      <SyntaxHighlighter language='javascript'>
+        {`module.exports = (apiRootUrl, apiEMail, apiPassword) => ({
   id: 'login',
   main: {
     method: 'post',
@@ -55,11 +55,11 @@ const AutoWiring = () => <Layout Outlet={<section>
     '^access-token',
   ],
 });
-`}
-    </SyntaxHighlighter>
-  </div>
-</section>}
+  `}
+      </SyntaxHighlighter>
+    </div>
+  </section>}
   page='autowiring'
   path='/usage/autowiring'
-/>
+/>;
 export default AutoWiring;
--- website/src/pages/usage/route/index.tsx
+++ website/src/pages/usage/route/index.tsx
@@ -5,30 +5,31 @@ import {
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import Layout from '../../../components/layout.tsx';
 
-const Route = () => <Layout Outlet={<section>
-  <div className='title-card'>
-    <h1><Lang lnkey='route.title'/></h1>
-    <p><Lang lnkey='route.description'/></p>
-  </div>
-  <div className='card'>
-    <h2><Lang lnkey='route.example.title'/></h2>
-    <div>
-      <p><Lang lnkey='route.example.description'/></p>
-      <SyntaxHighlighter language='typescript'>{`import {
-Task,
-} from '@idrinth/api-bench';
-
-export default (apiURL, apiPassword): Task => {
-main: {
-  method: 'get',
-  url: apiURL + '?' + apiPassword
-}
-};
-    `}</SyntaxHighlighter>
+const Route = () => <Layout
+  Outlet={<section>
+    <div className='title-card'>
+      <h1><Lang lnkey='route.title'/></h1>
+      <p><Lang lnkey='route.description'/></p>
     </div>
-  </div>
-</section>}
-  page='autowiring'
-  path='/usage/route'
+    <div className='card'>
+      <h2><Lang lnkey='route.example.title'/></h2>
+      <div>
+        <p><Lang lnkey='route.example.description'/></p>
+        <SyntaxHighlighter language='typescript'>{`import {
+  Task,
+} from '@idrinth/api-bench';
+  
+export default (apiURL, apiPassword): Task => {
+  main: {
+    method: 'get',
+    url: apiURL + '?' + apiPassword,
+  },
+};
+      `}</SyntaxHighlighter>
+      </div>
+    </div>
+  </section>}
+  page='route'
+  path='/usage/routes'
 />;
 export default Route;
