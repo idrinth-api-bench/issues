861cc592375702d47d9cad3eae4d70de1deecba0
Björn Büttner <bjoern.buettner@jungwild.io>
2024-01-11 16:23:38 +0100

    improvements to the tool

--- tools/checkout-and-install.js
+++ tools/checkout-and-install.js
@@ -1,28 +1,34 @@
 import {
   execSync,
 } from 'child_process';
+import readline from 'readline';
 
-const exec = (command) => {
+const exec = (command, passthrough=false) => {
   console.log(command);
-  return execSync(command);
+  return execSync(command, passthrough ? {stdio: 'inherit'} : {});
 };
 
 if (!process.argv[2]) {
-  console.error('username missing');
-  process.exit(1);
-}
-if (!process.argv[3]) {
-  console.error('branch missing');
-  process.exit(1);
-}
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  rl.question('username:branch missing, what do you want to checkout?', (userbranch) => {
+    exec('node '+process.cwd()+'/tools/checkout-and-install.js '+userbranch, true);
+    process.exit(0);
+  });
+} else {
+  const user = process.argv[2].includes(':') ? process.argv[2].split(':')[0] : 'idrinth';
+  const branch = process.argv[2].includes(':') ? process.argv[2].split(':')[1] : process.argv[2];
 
-const remotes = exec(`git remote`) + '';
-if (!remotes.includes(process.argv[2])) {
-  exec(`git remote add ${process.argv[2]} https://github.com/${process.argv[2]}/api-bench`);
+  const remotes = exec(`git remote`) + '';
+  if (!remotes.includes(user)) {
+    exec(`git remote add ${user} https://github.com/${user}/api-bench`, true);
+  }
+  exec(`git fetch ${user}`, true);
+  exec(`git checkout -B ${user}-${branch} --track ${user}/${branch}`, true);
+  exec(`git pull`, true);
+  exec('npm install', true);
+  exec('cd website && npm install', true);
+  exec('cd website && npm run build', true);
 }
-exec(`git fetch ${process.argv[2]}`);
-exec(`git checkout -B ${process.argv[2]}-${process.argv[3]} --track ${process.argv[2]}/${process.argv[3]}`);
-exec(`git pull`);
-exec('npm install');
-exec('cd website && npm install');
-exec('cd website && npm run build');
