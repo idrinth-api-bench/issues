6f5c02dc9c39bee008473b721ea09cf92084bf48
Björn Büttner <bjoern.buettner@jungwild.io>
2024-01-17 18:11:44 +0100

    early return

--- website/tools/load-contributors.js
+++ website/tools/load-contributors.js
@@ -16,29 +16,34 @@ if (existsSync('./src/contributors.json')) {
     }
   }
 }
-for(const contributor of await contributors.json()) {
-  if (contributor.type === 'User') {
-    if (users[contributor.login]) {
-      users[user.login].contributions = contributor.contributions;
-    } else {
-      const data = await fetch(contributor.url);
-      const user = await data.json();
-      const hash = crypto.createHash('md5').update(user.login).digest("hex");
-      users[user.login] = {
-        contributions: contributor.contributions,
-        name: user.name || user.login,
-        avatar: '/assets/profile-' + hash + '.jpg',
-        url: user.html_url,
-        bio: user.bio || 'An awesome person helping others in their time off work, who doesn\'t yet have a personalized bio.',
-        location: user.location,
-        lastUpdated: Date.now(),
-      };
-      writeFileSync(
-        './src/assets/profile-' + hash + '.jpg',
-        Buffer.from(new Uint8Array(await (await fetch(user.avatar_url)).arrayBuffer()))
-      );
-    }
+const update = async(contributor) => {
+  if (contributor.type !== 'User') {
+    return;
   }
+  if (users[contributor.login]) {
+    users[contributor.login].contributions = contributor.contributions;
+    return;
+  }
+  const data = await fetch(contributor.url);
+  const user = await data.json();
+  const hash = crypto.createHash('md5').update(user.login).digest("hex");
+  users[user.login] = {
+    contributions: contributor.contributions,
+    name: user.name || user.login,
+    avatar: '/assets/profile-' + hash + '.jpg',
+    url: user.html_url,
+    bio: user.bio || 'An awesome person helping others in their time off work, who doesn\'t yet have a personalized bio.',
+    location: user.location,
+    lastUpdated: Date.now(),
+  };
+  writeFileSync(
+    './src/assets/profile-' + hash + '.jpg',
+    Buffer.from(new Uint8Array(await (await fetch(user.avatar_url)).arrayBuffer()))
+  );
+}
+
+for(const contributor of await contributors.json()) {
+  update(contributor);
 }
 
 writeFileSync('./src/contributors.json', JSON.stringify(users));
