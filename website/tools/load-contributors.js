ddaa101e7a79a2361c2fc6c7d229c3f880c1084a
Björn Büttner <bjoern.buettner@jungwild.io>
2024-01-25 10:45:05 +0100

    add more caching don't load contributors in CI

--- website/tools/load-contributors.js
+++ website/tools/load-contributors.js
@@ -1,5 +1,6 @@
 import {
-  existsSync, mkdirSync,
+  existsSync,
+  mkdirSync,
   readFileSync,
   writeFileSync,
 } from 'fs';
@@ -14,56 +15,58 @@ const contributors = await fetch(
 );
 const users = {};
 
-if (existsSync('./src/contributors.json',)) {
-  const old = JSON.parse(readFileSync('./src/contributors.json', 'utf8',),);
-  for (const key of Object.keys(old,)) {
-    if (old[key].lastUpdated > Date.now() - MILLISECONDS_PER_DAY) {
-      users[key] = old[key];
+if (! process.env.CI) {
+  if (existsSync('./src/contributors.json',)) {
+    const old = JSON.parse(readFileSync('./src/contributors.json', 'utf8',),);
+    for (const key of Object.keys(old,)) {
+      if (old[key].lastUpdated > Date.now() - MILLISECONDS_PER_DAY) {
+        users[key] = old[key];
+      }
     }
   }
-}
-if (! existsSync('./public/assets',)) {
-  mkdirSync('./public/assets', {
-    recursive: true,
-  },);
-}
+  if (!existsSync('./public/assets',)) {
+    mkdirSync('./public/assets', {
+      recursive: true,
+    },);
+  }
 
-const update = async(contributor,) => {
-  if (contributor.type !== 'User') {
-    return;
-  }
-  if (users[contributor.login]) {
-    users[contributor.login].contributions = contributor.contributions;
-    return;
-  }
-  const data = await fetch(contributor.url);
-  const user = await data.json();
-  const hash = crypto
-    .createHash('md5',)
-    .update(user.login,)
-    .digest('hex',);
-  users[user.login] = {
-    contributions: contributor.contributions,
-    name: user.name || user.login,
-    avatar: '/assets/profile-' + hash + '.jpg',
-    url: user.html_url,
-    bio: user.bio || defaultBio,
-    location: user.location || 'unknown',
-    lastUpdated: Date.now(),
-  };
-  writeFileSync(
-    './public/assets/profile-' + hash + '.jpg',
-    Buffer.from(
-      new Uint8Array(
-        await (await fetch(user.avatar_url,)).arrayBuffer(),
+  const update = async (contributor,) => {
+    if (contributor.type !== 'User') {
+      return;
+    }
+    if (users[contributor.login]) {
+      users[contributor.login].contributions = contributor.contributions;
+      return;
+    }
+    const data = await fetch(contributor.url);
+    const user = await data.json();
+    const hash = crypto
+      .createHash('md5',)
+      .update(user.login,)
+      .digest('hex',);
+    users[user.login] = {
+      contributions: contributor.contributions,
+      name: user.name || user.login,
+      avatar: '/assets/profile-' + hash + '.jpg',
+      url: user.html_url,
+      bio: user.bio || defaultBio,
+      location: user.location || 'unknown',
+      lastUpdated: Date.now(),
+    };
+    writeFileSync(
+      './public/assets/profile-' + hash + '.jpg',
+      Buffer.from(
+        new Uint8Array(
+          await (await fetch(user.avatar_url,)).arrayBuffer(),
+        ),
       ),
-    ),
-  );
-};
+    );
+  };
 
-for (const contributor of await contributors.json()) {
-  // eslint-disable-next-line no-await-in-loop
-  await update(contributor,);
+  for (const contributor of await contributors.json()) {
+    // eslint-disable-next-line no-await-in-loop
+    await update(contributor,);
+  }
 }
 
 writeFileSync('./src/contributors.json', JSON.stringify(users,),);
