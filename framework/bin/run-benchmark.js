8dbc925cf4fbac9855ecd14ae0e2d1167a5a423f
Madina <madinatussupova0820@gmail.com>
2024-01-30 09:12:00 +0600

    add a command line option to only run a specific case id

--- framework/bin/run-benchmark.js
+++ framework/bin/run-benchmark.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
-import {cli, run} from '../src/cli/bench.js';
+import {
+  cli, run,
+} from '../src/cli/bench.js';
 
 cli(process.argv,);
 
-const args = process.argv.slice(2);
-const filterId = args.find(arg => arg.startsWith('--filterId='));
-const filterIdValue = filterId ? filterId.split('=')[1] : null;
+const args = process.argv.slice(2,);
+const filterId = args.find(function(arg,) {
+  return arg.startsWith('--taskId',);
+},);
+const filterIdValue = filterId ? filterId.split('=',)[1] : null;
 
-await run({taskId: filterIdValue});
+await run({
+  taskId: filterIdValue,
+},);
--- framework/src/build-task-list.ts
+++ framework/src/build-task-list.ts
@@ -1,23 +1,21 @@
-import { Task } from "./task.js";
+import {
+  Task,
+} from './task.js';
 
-export default (tasks: Task[], blacklist: string[], total: number) => {
+export default (tasks: Task[], blacklist: string[], total: number,) => {
   const internalTasks = [];
-
   for (const task of tasks) {
-    // Check if the task is a main task (has 'main' property and no 'pre' or 'post')
-    if (task.main && !task.pre && !task.post) {
-      for (let i = 0; i < total; i++) {
-        // Create a new task object with a shallow copy
-        const mainTask = { ...task, pre: undefined, post: undefined };
-        internalTasks.push(mainTask);
+    if (task.main) {
+      if (task.pre) {
+        task.pre = task.pre.filter((entry,) => ! blacklist.includes(entry,),);
       }
-    } else {
-      // Add the task to the internalTasks array
-      for (let i = 0; i < total; i++) {
-        internalTasks.push(task);
+      if (task.post) {
+        task.post = task.post.filter((entry,) => ! blacklist.includes(entry,),);
+      }
+      for (let i = 0; i < total; i ++) {
+        internalTasks.push(task,);
       }
     }
   }
-
   return internalTasks;
 };
