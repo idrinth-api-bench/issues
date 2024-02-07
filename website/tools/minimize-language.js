0ed5540329c03a0a863b38affd3f9b470853629c
Madina <101329759+madina0801@users.noreply.github.com>
2024-02-07 18:20:47 +0600

    Generate the website translation jsons from yaml sources (#277)
    * sort contributors by number of contributions
    
    * add a command line option to only run a specific case id
    
    * add a command line option to only run a specific case id
    
    * add a command line option to only run a specific case id
    
    * introduce filtering
    add unit test
    
    * yaml minification
    
    * #195 second attempt
    
    * filter yaml files
    
    * remove catch
    
    * changes made
    
    ---------
    
    Co-authored-by: Björn Büttner <Idrinth@users.noreply.github.com>
    Co-authored-by: Björn Büttner <bjoern.buettner@jungwild.io>

--- website/tools/minimize-language.js
+++ website/tools/minimize-language.js
@@ -3,11 +3,31 @@ import {
   readFileSync,
   writeFileSync,
   existsSync,
+  mkdirSync
 } from 'fs';
 
-for (const folder of readdirSync('./dist/locales', 'utf8')) {
-    if (existsSync('./dist/locales/' + folder + '/translation.json')) {
-      const content = readFileSync('./dist/locales/' + folder + '/translation.json', 'utf8',);
-      writeFileSync('./dist/locales/' + folder + '/translation.json', JSON.stringify(JSON.parse(content,),),);
+import yaml from 'yaml';
+import { dirname } from 'path';
+
+const generateJSONFromYAML = (yamlPath, outputPath) => {
+  const content = readFileSync(yamlPath, 'utf8');
+  const jsonData = yaml.parse(content);
+  writeFileSync(outputPath, JSON.stringify(jsonData));
+};
+
+const originDir = 'language';
+const targetDir = 'public/locales';
+
+const yamlFiles = readdirSync('language').filter(file => file.endsWith('.yml'));
+
+yamlFiles.forEach((yamlFile) => {
+  const lang = yamlFile.replace('.yml', '');
+  const yamlPath = `${originDir}/${yamlFile}`;
+  const outputPath = `${targetDir}/${lang}/translation.json`;
+
+  if (!existsSync(dirname(outputPath))) {
+    mkdirSync(dirname(outputPath), { recursive: true });
   }
-}
+
+  generateJSONFromYAML(yamlPath, outputPath);
+});
