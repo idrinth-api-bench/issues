d34b3e4f14a6fea15ea950cdb90a83378679b500
Madina <madinatussupova0820@gmail.com>
2024-02-07 16:43:30 +0600

    changes made

--- website/tools/minimize-language.js
+++ website/tools/minimize-language.js
@@ -3,23 +3,22 @@ import {
   readFileSync,
   writeFileSync,
   existsSync,
+  mkdirSync
 } from 'fs';
 
 import yaml from 'yaml';
 import { dirname } from 'path';
 
 const generateJSONFromYAML = (yamlPath, outputPath) => {
-  if (existsSync(yamlPath)) {
-    const content = readFileSync(yamlPath, 'utf8');
-    const jsonData = yaml.parse(content);
-    writeFileSync(outputPath, JSON.stringify(jsonData));
-  }
+  const content = readFileSync(yamlPath, 'utf8');
+  const jsonData = yaml.parse(content);
+  writeFileSync(outputPath, JSON.stringify(jsonData));
 };
 
 const originDir = 'language';
 const targetDir = 'public/locales';
 
-const yamlFiles = readdirSync('language').filter(file => /\.yml$/.test(file));
+const yamlFiles = readdirSync('language').filter(file => file.endsWith('.yml'));
 
 yamlFiles.forEach((yamlFile) => {
   const lang = yamlFile.replace('.yml', '');
@@ -27,17 +26,8 @@ yamlFiles.forEach((yamlFile) => {
   const outputPath = `${targetDir}/${lang}/translation.json`;
 
   if (!existsSync(dirname(outputPath))) {
-    // create directory if it's not here
     mkdirSync(dirname(outputPath), { recursive: true });
   }
 
   generateJSONFromYAML(yamlPath, outputPath);
 });
-
-// Kept for myself to compare
-// for (const folder of readdirSync('./dist/locales', 'utf8')) {
-//     if (existsSync('./dist/locales/' + folder + '/translation.json')) {
-//       const content = readFileSync('./dist/locales/' + folder + '/translation.json', 'utf8',);
-//       writeFileSync('./dist/locales/' + folder + '/translation.json', JSON.stringify(JSON.parse(content,),),);
-//   }
-// }
