96125b171bb0aae50a1b2990883d74bcdcd469b3
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-02-11 01:42:10 -0700

    formatting, spaces

--- website/cypress.config.ts
+++ website/cypress.config.ts
@@ -1,16 +1,16 @@
 import {
-    defineConfig,
+  defineConfig,
 } from 'cypress';
 import task from '@cypress/code-coverage/task';
 import useBabelRC from '@cypress/code-coverage/use-babelrc';
 
 export default defineConfig({
-    e2e: {
-        setupNodeEvents(on, config,) {
-            task(on, config,);
-            on('file:preprocessor', useBabelRC,);
-            return config;
-        },
-        baseUrl: 'http://localhost:8080'
+  e2e: {
+    setupNodeEvents(on, config,) {
+      task(on, config,);
+      on('file:preprocessor', useBabelRC,);
+      return config;
     },
+    baseUrl: 'http://localhost:8080'
+  },
 },);
