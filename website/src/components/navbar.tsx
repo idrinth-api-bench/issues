c46b8794de150aada6779ca8aa58ee6e1ee695a2
aditya bhate <98549181+AdityaBhate@users.noreply.github.com>
2024-02-19 12:49:16 +0530

    #297 add linkedin group to navigation (#335)

--- website/src/components/navbar.tsx
+++ website/src/components/navbar.tsx
@@ -14,18 +14,18 @@ import ExternalLink from './external-link.tsx';
 const Navbar = () => <>
   <header>
     <div>
-      <img src={IAB} alt="@idrinth/api-bench"/>
+      <img src={IAB} alt="@idrinth/api-bench" />
       <strong>@idrinth/api-bench</strong>
       <em>v{pkg.version}</em>
     </div>
     <nav>
       <ul>
         <li>
-          <NavLink to="/"><Lang lnkey='home.nav'/></NavLink>
+          <NavLink to="/"><Lang lnkey='home.nav' /></NavLink>
         </li>
         <li>
           <NavLink to="/quick-start/">
-            <Lang lnkey='quick-start.nav'/>
+            <Lang lnkey='quick-start.nav' />
           </NavLink>
         </li>
         <li>
@@ -41,55 +41,61 @@ const Navbar = () => <>
           />
         </li>
         <li>
+          <ExternalLink
+            to="https://www.linkedin.com/groups/9588634/"
+            label={'LINKEDIN'}
+          />
+        </li>
+        <li>
           <NavLink to="/contributing/">
-            <Lang lnkey='contributing.nav'/>
+            <Lang lnkey='contributing.nav' />
           </NavLink>
           <ul>
             <li>
               <NavLink to="/contributing/contributors/">
-                <Lang lnkey='contributors.nav'/>
+                <Lang lnkey='contributors.nav' />
               </NavLink>
             </li>
             <li>
               <NavLink to="/contributing/sponsors/">
-                <Lang lnkey='sponsors.nav'/>
+                <Lang lnkey='sponsors.nav' />
               </NavLink>
             </li>
           </ul>
         </li>
         <li>
           <NavLink to="/usage/">
-            <Lang lnkey='usage.nav'/>
+            <Lang lnkey='usage.nav' />
           </NavLink>
           <ul>
             <li>
               <NavLink to="/usage/autowiring/">
-                <Lang lnkey='autowiring.nav'/>
+                <Lang lnkey='autowiring.nav' />
               </NavLink>
             </li>
             <li>
               <NavLink to="/usage/results/">
-                <Lang lnkey='results.nav'/>
+                <Lang lnkey='results.nav' />
               </NavLink>
             </li>
             <li>
               <NavLink to="/usage/logging/">
-                <Lang lnkey='logging.nav'/>
+                <Lang lnkey='logging.nav' />
               </NavLink>
             </li>
             <li>
               <NavLink to="/usage/middlewares/">
-                <Lang lnkey='middlewares.nav'/>
+                <Lang lnkey='middlewares.nav' />
               </NavLink>
             </li>
             <li>
               <NavLink to="/usage/storage/">
-                <Lang lnkey='storage.nav'/>
+                <Lang lnkey='storage.nav' />
               </NavLink>
             </li>
             <li>
               <NavLink to="/usage/routes/">
-                <Lang lnkey='routes.nav'/>
+                <Lang lnkey='routes.nav' />
               </NavLink>
             </li>
           </ul>
