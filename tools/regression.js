5082519f74c4c2a3adbde29a728206c97c2e4b80
Björn Büttner <bjoern.buettner@jungwild.io>
2024-02-22 00:58:03 +0100

    eslint fixes

--- tools/regression.js
+++ tools/regression.js
@@ -7,42 +7,52 @@ import {
   rmdirSync,
   mkdirSync,
   statSync,
-  existsSync
+  existsSync,
 } from 'fs';
 
-const copy = (from, to) => {
-  if (!existsSync(process.cwd() + to)) {
-    mkdirSync(process.cwd() + to);
+const copy = (from, to,) => {
+  if (! existsSync(process.cwd() + to,)) {
+    mkdirSync(process.cwd() + to,);
   }
-  for (const file of readdirSync(process.cwd() + from, {encoding: 'utf8'})) {
-    const data = statSync(process.cwd() + from);
+  for (const file of readdirSync(process.cwd() + from, {
+    encoding: 'utf8',
+  },)) {
+    const data = statSync(process.cwd() + from,);
     if (data.isDirectory()) {
-      copy(from + '/' + file, to + '/' + file);
+      copy(from + '/' + file, to + '/' + file,);
     } else {
-      writeFileSync(process.cwd() + to + '/' + file, readFileSync(process.cwd() + from + '/' + file, 'utf8'), 'utf8');
+      writeFileSync(
+        process.cwd() + to + '/' + file,
+        readFileSync(process.cwd() + from + '/' + file, 'utf8',),
+        'utf8',
+      );
     }
   }
-}
-const rm = (dir) => {
-  if (!existsSync(process.cwd() + dir)) {
+};
+const rm = (dir,) => {
+  if (! existsSync(process.cwd() + dir,)) {
     return;
   }
-  for (const file of readdirSync(process.cwd() + dir, {encoding: 'utf8'})) {
-    const data = statSync(process.cwd() + dir);
+  for (const file of readdirSync(process.cwd() + dir, {
+    encoding: 'utf8',
+  },)) {
+    const data = statSync(process.cwd() + dir,);
     if (data.isDirectory()) {
-      rm(dir + '/' + file);
+      rm(dir + '/' + file,);
     } else {
-      unlinkSync(process.cwd() + dir + '/' + file);
+      unlinkSync(process.cwd() + dir + '/' + file,);
     }
   }
-  rmdirSync(process.cwd() + dir);
-}
+  rmdirSync(process.cwd() + dir,);
+};
 
 exec('cd framework && npm install && npm run tsc', true,);
-for (const folder of readdirSync(process.cwd() + '/examples', {encoding: 'utf8'})) {
+for (const folder of readdirSync(process.cwd() + '/examples', {
+  encoding: 'utf8',
+},)) {
   if (folder !== '.eslintrc.yml') {
     exec(`cd examples/${ folder } && npm install`, true,);
-    rm(`/examples/${ folder }/node_modules/@idrinth/api-bench`);
-    copy('/framework', `/examples/${ folder }`);
+    rm(`/examples/${ folder }/node_modules/@idrinth/api-bench`,);
+    copy('/framework', `/examples/${ folder }`,);
   }
 }
