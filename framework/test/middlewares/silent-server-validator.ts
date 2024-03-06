6db0c9a0288e34b4d7060c337fcb27cc266f63d6
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-07 00:12:17 +0100

    fixes #356 add validator to check for info leakage (#387)

---
+++ framework/test/middlewares/silent-server-validator.ts
@@ -0,0 +1,87 @@
+import SilentServerValidator from '../../src/middlewares/silent-server-validator.js';
+import {
+  expect,
+} from 'chai';
+import 'mocha';
+import {
+  NeedleHttpVerbs,
+} from 'needle';
+import {Result} from "../../src/result";
+
+describe('middlewares/silent-server-validator', () => {
+  it('should be a class', () => {
+    expect(SilentServerValidator,).to.be.a('function',);
+  },);
+  it('should have a static method prepare', () => {
+    expect(SilentServerValidator.prepare,).to.be.a('function',);
+  },);
+  it('should have a static method process', () => {
+    expect(SilentServerValidator.process,).to.be.a('function',);
+  },);
+  it('process should not throw if there are no headers', () => {
+    const input = {
+      response: {
+        // eslint-disable-next-line no-undefined
+        status: undefined,
+        headers: {},
+        cookies: {},
+        body: '',
+        uri: '',
+      },
+      duration: 0,
+      id: '',
+      validators: [],
+    };
+    expect(() => SilentServerValidator.process(input as Result,),).to.not.throw();
+  },);
+  it('process should not throw if there is a Server header', () => {
+    const input = {
+      response: {
+        // eslint-disable-next-line no-undefined
+        status: undefined,
+        headers: {
+          Server: 'nginx'
+        },
+        cookies: {},
+        body: '',
+        uri: '',
+      },
+      duration: 0,
+      id: '',
+      validators: [],
+    };
+    expect(() => SilentServerValidator.process(input as Result,),).to.throw(
+      'The header Server is set. Remove this'
+    );
+  },);
+  it('process should not throw if there is a X-Powered-By header', () => {
+    const input = {
+      response: {
+        // eslint-disable-next-line no-undefined
+        status: undefined,
+        headers: {
+          'X-Powered-By': 'php/8.1.2'
+        },
+        cookies: {},
+        body: '',
+        uri: '',
+      },
+      duration: 0,
+      id: '',
+      validators: [],
+    };
+    expect(() => SilentServerValidator.process(input as Result,),).to.throw(
+      'The header X-Powered-By is set. It shares critical information with the world.',
+    );
+  },);
+  it('prepare should return input', () => {
+    const input = {
+      method: 'head' as NeedleHttpVerbs,
+      headers: {},
+      cookies: {},
+      body: 'body',
+      url: 'url',
+    };
+    expect(SilentServerValidator.prepare(input,),).to.equal(input,);
+  },);
+},);
