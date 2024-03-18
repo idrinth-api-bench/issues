e6e784fddaff724080ed194f4aaf245bcd995a74
Stephanie Vizzi <stephanievizzi@gmail.com>
2024-03-18 05:53:10 -0400

    Allow user to set light/dark mode - default: user settings

--- website/src/components/navbar.tsx
+++ website/src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import IAB from '../assets/iab.svg';
 import {
   NavLink,
@@ -11,97 +11,124 @@ import {
 } from './lang.tsx';
 import ExternalLink from './external-link.tsx';
 
-const Navbar = () => <>
-  <header>
-    <div>
-      <img src={IAB} alt="@idrinth/api-bench" />
-      <strong>@idrinth/api-bench</strong>
-      <em>v{pkg.version}</em>
-    </div>
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/"><Lang lnkey='home.nav' /></NavLink>
-        </li>
-        <li>
-          <NavLink to="/quick-start/">
-            <Lang lnkey='quick-start.nav' />
-          </NavLink>
-        </li>
-        <li>
-          <ExternalLink
-            to="https://www.npmjs.com/package/@idrinth/api-bench"
-            label={'NPMJS'}
-          />
-        </li>
-        <li>
-          <ExternalLink
-            to="https://github.com/Idrinth/api-bench"
-            label={'GITHUB'}
-          />
-        </li>
-        <li>
-          <ExternalLink
-            to="https://www.linkedin.com/groups/9588634/"
-            label={'LINKEDIN'}
-          />
-        </li>
-        <li>
-          <NavLink to="/contributing/">
-            <Lang lnkey='contributing.nav' />
-          </NavLink>
-          <ul>
-            <li>
-              <NavLink to="/contributing/contributors/">
-                <Lang lnkey='contributors.nav' />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/contributing/sponsors/">
-                <Lang lnkey='sponsors.nav' />
-              </NavLink>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <NavLink to="/usage/">
-            <Lang lnkey='usage.nav' />
-          </NavLink>
-          <ul>
-            <li>
-              <NavLink to="/usage/autowiring/">
-                <Lang lnkey='autowiring.nav' />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/usage/results/">
-                <Lang lnkey='results.nav' />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/usage/logging/">
-                <Lang lnkey='logging.nav' />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/usage/middlewares/">
-                <Lang lnkey='middlewares.nav' />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/usage/storage/">
-                <Lang lnkey='storage.nav' />
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/usage/routes/">
-                <Lang lnkey='routes.nav' />
-              </NavLink>
-            </li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
-  </header>
-</>;
+
+const Navbar = () => {
+  const setInitialDarkMode = (): any => {
+    let localStorageValue = localStorage.getItem("dark-mode");
+    if (!localStorageValue) {
+      let darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+      localStorage.setItem("dark-mode", JSON.stringify(darkMode))
+      return darkMode
+    } else {
+      return JSON.parse(localStorageValue)
+    }
+  }
+
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(setInitialDarkMode())
+
+  const toggleLightDarkMode = () => {
+    localStorage.setItem('dark-mode', JSON.stringify(!isDarkMode));
+    setIsDarkMode(!isDarkMode)
+  }
+
+  useEffect(() => {
+    document.getElementsByTagName('html')[0].classList.add(isDarkMode ? "dark-mode" : "light-mode")
+    document.getElementsByTagName('html')[0].classList.remove(!isDarkMode ? "dark-mode" : "light-mode")
+  }, [isDarkMode]);
+
+  return <>
+    <header>
+      <div>
+        <img src={IAB} alt="@idrinth/api-bench" />
+        <strong>@idrinth/api-bench</strong>
+        <em>v{pkg.version}</em>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/"><Lang lnkey='home.nav' /></NavLink>
+          </li>
+          <li>
+            <NavLink to="/quick-start/">
+              <Lang lnkey='quick-start.nav' />
+            </NavLink>
+          </li>
+          <li>
+            <ExternalLink
+              to="https://www.npmjs.com/package/@idrinth/api-bench"
+              label={'NPMJS'}
+            />
+          </li>
+          <li>
+            <ExternalLink
+              to="https://github.com/Idrinth/api-bench"
+              label={'GITHUB'}
+            />
+          </li>
+          <li>
+            <ExternalLink
+              to="https://www.linkedin.com/groups/9588634/"
+              label={'LINKEDIN'}
+            />
+          </li>
+          <li>
+            <NavLink to="/contributing/">
+              <Lang lnkey='contributing.nav' />
+            </NavLink>
+            <ul>
+              <li>
+                <NavLink to="/contributing/contributors/">
+                  <Lang lnkey='contributors.nav' />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/contributing/sponsors/">
+                  <Lang lnkey='sponsors.nav' />
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <NavLink to="/usage/">
+              <Lang lnkey='usage.nav' />
+            </NavLink>
+            <ul>
+              <li>
+                <NavLink to="/usage/autowiring/">
+                  <Lang lnkey='autowiring.nav' />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/usage/results/">
+                  <Lang lnkey='results.nav' />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/usage/logging/">
+                  <Lang lnkey='logging.nav' />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/usage/middlewares/">
+                  <Lang lnkey='middlewares.nav' />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/usage/storage/">
+                  <Lang lnkey='storage.nav' />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/usage/routes/">
+                  <Lang lnkey='routes.nav' />
+                </NavLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <button onClick={toggleLightDarkMode}>light/dark</button>
+      </nav>
+    </header>
+  </>;
+}
 export default Navbar;
