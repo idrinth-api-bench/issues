5f7f452f436c3d5038a8d5ef1d41fb62e24be40b
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-23 00:20:33 +0100

    add a test(insufficient)

---
+++ framework/test/cli/check-routes.ts
@@ -0,0 +1,11 @@
+import cli from '../../src/cli/check-routes.js';
+import {
+  expect,
+} from 'chai';
+import 'mocha';
+
+describe('check-routes', () => {
+  it('should be a function', () => {
+    expect(cli,).to.be.a('function',);
+  },);
+},);
