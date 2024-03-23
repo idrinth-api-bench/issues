256a935cf5e54a305b0b38ed6a1016f489c076ec
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-23 01:13:35 +0100

    feature(framework): prepare validation (#446)
    * feature(framework): prepare validation
    
    fixes #332

---
+++ framework/src/task-types.ts
@@ -0,0 +1,12 @@
+import taskType from './task-type.js';
+
+const taskTypes: taskType[] = [
+  'before',
+  'main',
+  'after',
+  'beforeAll',
+  'afterAll',
+  'beforeEach',
+  'afterEach',
+];
+export default taskTypes;
