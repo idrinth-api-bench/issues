abf08e76259e95281e8071b09d826f50c27a2086
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-23 12:20:29 +0100

    feature(documentation-website): translate contributing

--- documentation-website/src/pages/contributing/index.tsx
+++ documentation-website/src/pages/contributing/index.tsx
@@ -1,89 +1,74 @@
 import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import Layout from '../../components/layout.tsx';
-import engine from '../../../../package.json' with {
+import {
+  engines,
+} from '../../../../package.json' with {
   type: 'json'
 };
 import './contributing.css';
+import {
+  Lang,
+} from '../../components/lang.tsx';
 
 const Index = () => <Layout
   Outlet={<section>
     <div className='title-card'>
       <h1>
-        Contributing
+        <Lang lnkey={'contributing.title'}/>
       </h1>
       <p>
-        Any and all contributions are encouraged.
+        <Lang lnkey={'contributing.description'}/>
       </p>
     </div>
     <div className='card'>
-      <h2>Structure</h2>
+      <h2><Lang lnkey={'contributing.structure.title'}/></h2>
       <p>
-        The project is a mono-repo grouped into multiple subprojects, for
-        example website, framework, history-microservice and history-website.
-        You can find more detailed instructions for the specific part in the
-        corresponding folder&apos;s readme.
+        <Lang lnkey={'contributing.structure.description'}/>
       </p>
     </div>
     <div className='card'>
-      <h2>Issues</h2>
+      <h2><Lang lnkey={'contributing.issues.title'}/></h2>
       <p>
-        Any idea and bug report is considered a contribution.
-        Not only do they help improving the code base,
-        they help other people to get more use out of this framework.
-        Please try to stick to the format of predefined issue types
-        to make it easier to filter and handle for anyone interested in the
-        topic.
+        <Lang lnkey={'contributing.issues.description'}/>
       </p>
     </div>
     <div className='card'>
-      <h2>Code</h2>
+      <h2><Lang lnkey={'contributing.code.title'}/></h2>
       <div>
         <p>
-          The style guidelines are provided via eslint.
-          Please try to optimise code for readability,
-          since code will be read way more often than it will be changed.
+          <Lang lnkey={'contributing.code.style'}/>
         </p>
         <p>
-          While adding functionality, keep in mind to break existing
-          interfaces, entry points and functionality only if absolutely
-          necessary.
+          <Lang lnkey={'contributing.code.style'}/>
         </p>
       </div>
     </div>
     <div className='card'>
-      <h3>Contribution workflow</h3>
+      <h3><Lang lnkey={'contributing.code.workflow.title'}/></h3>
       <ul>
-        <li>fork the repository</li>
-        <li>create a feature branch (issue-xxx-does-something) from master</li>
-        <li>create a draft pull request</li>
-        <li>mention fixes #xxx in the description</li>
-        <li>make your changes to the branch</li>
-        <li>check if actions are all green</li>
-        <li>mark as ready to review</li>
-        <li>get changes reviewed</li>
-        <li>
-          if review is positive get changes merged otherwise fix and
-          send back to review
-        </li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_1'}/></li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_2'}/></li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_3'}/></li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_4'}/></li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_5'}/></li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_6'}/></li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_7'}/></li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_8'}/></li>
+        <li><Lang lnkey={'contributing.code.workflow.list_item_9'}/></li>
       </ul>
     </div>
     <div className='card'>
-      <h3>Tests</h3>
+      <h3><Lang lnkey={'contributing.code.tests.title'}/></h3>
       <p>
-        Testing new code is expected and adding tests or
-        improving tests for old code is appreciated.
-        The goal is to find most issues by either
-        static analysis or unit and integration tests.
+        <Lang lnkey={'contributing.code.tests.description'}/>
       </p>
     </div>
     <div className='card'>
-      <h4>Manual Tests</h4>
+      <h4><Lang lnkey={'contributing.code.tests.manual.title'}/></h4>
       <div>
         <p>
-          For manual testing you can run <strong>npm run coai</strong>{' '}
-          in the source folder to automatically check out and install
-          all required files for testing.
+          <Lang lnkey={'contributing.code.tests.manual.description'}/>
         </p>
         <SyntaxHighlighter language='bash'>{`
 git clone https://github.com/idrinth/api-bench
@@ -93,31 +78,31 @@ npm run coai
       </div>
     </div>
     <div className='card'>
-      <h3>Naming conventions</h3>
+      <h3><Lang lnkey={'contributing.code.naming.title'}/></h3>
       <ul>
-        <li>camelCase for variables and functions</li>
-        <li>UPPER_SNAKE_CASE for constants</li>
-        <li>PascalCase for interfaces and classes</li>
-        <li>kebab-case for file names</li>
+        <li><Lang lnkey={'contributing.code.naming.list_item_1'}/></li>
+        <li><Lang lnkey={'contributing.code.naming.list_item_2'}/></li>
+        <li><Lang lnkey={'contributing.code.naming.list_item_3'}/></li>
+        <li><Lang lnkey={'contributing.code.naming.list_item_4'}/></li>
       </ul>
     </div>
     <div className='card'>
-      <h3>Requirements</h3>
+      <h3><Lang lnkey={'contributing.code.naming.list_item_4'}/></h3>
       <ul>
         <li>
-          One of the last two node versions
-          {engine.engines.node}
+          <Lang lnkey={'contributing.code.requirements.node'}/>
+          {engines.node}
         </li>
         <li>
-          OS: Linux / Windows <i>(Other OS were not tested)</i>
+          <Lang lnkey={'contributing.code.requirements.os'}/>
         </li>
       </ul>
     </div>
     <div className='card'>
-      <h3>Setup</h3>
+      <h3><Lang lnkey={'contributing.code.setup.title'}/></h3>
       <div>
         <p>
-          Run the following commands to create the full project:
+          <Lang lnkey={'contributing.code.setup.description'}/>
         </p>
         <SyntaxHighlighter language='bash'>{`
   git clone https://github.com/idrinth/api-bench
@@ -127,16 +112,16 @@ npm run coai
       </div>
     </div>
     <div className="card">
-      <h2>Commit Message Guidelines</h2>
+      <h3><Lang lnkey={'contributing.code.commits.title'}/></h3>
       <div>
-        <p className="preface">*This specification is inspired by Angular commit
-          messages guidelines.
+        <p className="preface">
+          <Lang lnkey={'contributing.code.commits.preface'}/>
         </p>
-        <p>These are the rules for how Git commit messages for api-bench should
-          be formatted. This format leads to easier to read commit history.
+        <p>
+          <Lang lnkey={'contributing.code.commits.introduction'}/>
         </p>
-        <p>Each commit message consists of a <strong>header</strong>,
-          a <strong>body</strong>, and a <strong>footer</strong>.
+        <p>
+          <Lang lnkey={'contributing.code.commits.parts'}/>
         </p>
         <SyntaxHighlighter language='markdown'>{`
 <header>
@@ -145,106 +130,119 @@ npm run coai
 <BLANK LINE>
 <footer>
         `}</SyntaxHighlighter>
-        <p>The <span className="mono-text">header</span> is mandatory and
-          should conform to the Commit Message Header format.
-        </p>
-        <p>
-          The <span className="mono-text">body</span> is mandatory for
-          all commits except for those of type &quot;docs&quot; or in cases when
-          it&apos;s clear from the header summary. They should conform to the
-          Commit Message Body format.
-        </p>
-        <p>
-          The <span className="mono-text">footer</span> is optional.
-          The Commit Message Footer format describes what the footer is used
-          for and the structure it must have.
-        </p>
 
-        <h3>Commit Message Header</h3>
-        <p>Headers must adhere to the following format:</p>
-        <SyntaxHighlighter language='bash'>{`
+        <h4><Lang lnkey={'contributing.code.commits.header.title'}/></h4>
+        <p>
+          <Lang lnkey={'contributing.code.commits.header.description'}/>
+        </p>
+        <p>
+          <Lang lnkey={'contributing.code.commits.header.format'}/>
+        </p>
+        <SyntaxHighlighter language='markdown'>{`
 <type>(<scope>): <short summary>
 
 Example:
 build(website): bump follow-redirects from 1.15.4 to 1.15.6
         `}</SyntaxHighlighter>
-        <h4>Type</h4>
-        <p>The list of supported types:</p>
+        <h5><Lang lnkey={'contributing.code.commits.header.type.title'}/></h5>
+        <p>
+          <Lang lnkey={'contributing.code.commits.header.type.description'}/>
+        </p>
         <ul className="card-list">
           <li>
-            <span className="mono-text">build</span>: Changes that affect
-            the build system or external dependencies
+            <Lang lnkey={'contributing.code.commits.header.type.list_item_1'}/>
           </li>
           <li>
-            <span className="mono-text">ci</span>: Changes to CI configuration
-            files and scripts
+            <Lang lnkey={'contributing.code.commits.header.type.list_item_2'}/>
           </li>
           <li>
-            <span className="mono-text">docs</span>: Documentation changes
+            <Lang lnkey={'contributing.code.commits.header.type.list_item_3'}/>
           </li>
           <li>
-            <span className="mono-text">feature</span>: A new feature
+            <Lang lnkey={'contributing.code.commits.header.type.list_item_4'}/>
           </li>
           <li>
-            <span className="mono-text">fix</span>: A bug fix
+            <Lang lnkey={'contributing.code.commits.header.type.list_item_5'}/>
           </li>
           <li>
-            <span className="mono-text">perf</span>: A code change that
-            improves performance
+            <Lang lnkey={'contributing.code.commits.header.type.list_item_6'}/>
           </li>
           <li>
-            <span className="mono-text">refactor</span>: A code change
-            that neither fixes a bug nor adds a feature
+            <Lang lnkey={'contributing.code.commits.header.type.list_item_7'}/>
           </li>
           <li>
-            <span className="mono-text">test</span>: Adding missing tests
-            or correcting existing tests
+            <Lang lnkey={'contributing.code.commits.header.type.list_item_8'}/>
           </li>
         </ul>
-        <h4>Scope</h4>
-        <p>The scope should be the name of the affected part of the project.
-          The list of supported scopes:</p>
+        <h5>
+          <Lang lnkey={'contributing.code.commits.header.scope.title'}/>
+        </h5>
+        <p>
+          <Lang lnkey={'contributing.code.commits.header.scope.description'}/>
+        </p>
         <ul className="card-list">
-          <li><span className="mono-text">framework</span></li>
-          <li><span className="mono-text">website</span></li>
-          <li><span className="mono-text">history-microservice</span></li>
-          <li><span className="mono-text">history-website</span></li>
+          <li>
+            <Lang lnkey={'contributing.code.commits.header.scope.list_item_1'}/>
+          </li>
+          <li>
+            <Lang lnkey={'contributing.code.commits.header.scope.list_item_2'}/>
+          </li>
+          <li>
+            <Lang lnkey={'contributing.code.commits.header.scope.list_item_3'}/>
+          </li>
+          <li>
+            <Lang lnkey={'contributing.code.commits.header.scope.list_item_4'}/>
+          </li>
         </ul>
-        <h4>Summary</h4>
-        <p>The summary field should provide a succinct description of
-          the change.</p>
+        <h5>
+          <Lang lnkey={'contributing.code.commits.header.summary.list_item_4'}/>
+        </h5>
+        <p>
+          <Lang lnkey={'contributing.code.commits.header.summary.list_item_4'}/>
+        </p>
         <ul className="card-list">
-          <li>use the imperative, present tense</li>
-          <li>don&apos;t capitalize the first letter</li>
-          <li>no dot (.) at the end</li>
+          <li>
+            <Lang lnkey={'contributing.code.commits.header.summary.list_item_4'}/>
+          </li>
+          <li>
+            <Lang lnkey={'contributing.code.commits.header.summary.list_item_4'}/>
+          </li>
+          <li>
+            <Lang lnkey={'contributing.code.commits.header.summary.list_item_4'}/>
+          </li>
         </ul>
         <SyntaxHighlighter language='markdown'>{`
 // Bad:
-Fixes unit tests failing.
+Fixes bug.
 
 // Good:
 fix unit tests failing
         `}</SyntaxHighlighter>
 
-        <h3>Commit Message Body</h3>
-        <p>Just as in the summary, use the imperative, present tense.</p>
-        <p>The commit message body should explain why you are making
-          the change. You can include a comparison of the previous behavior
-          with the new behavior in order to illustrate the impact of the change.
+        <h4>
+          <Lang lnkey={'contributing.code.commits.body.title'}/>
+        </h4>
+        <p>
+          <Lang lnkey={'contributing.code.commits.body.description'}/>
+        </p>
+        <p>
+          <Lang lnkey={'contributing.code.commits.body.time'}/>
+        </p>
+        <p>
+          <Lang lnkey={'contributing.code.commits.body.content'}/>
         </p>
 
-        <h3>Commit Message Footer</h3>
+        <h4>
+          <Lang lnkey={'contributing.code.commits.footter.title'}/>
+        </h4>
         <p>
-          The footer can contain information about breaking changes and
-          deprecations and is also the place to reference GitHub issues and
-          other PRs that this commit closes or is related to.
+          <Lang lnkey={'contributing.code.commits.footer.description'}/>
         </p>
         <p>
-          In case of
-          breaking changes and deprecations the section should start with
-          the phrase &quot;BREAKING CHANGE: &quot; or
-          &quot;DEPRECATED: &quot; respectively followed by a description.
-          For example:
+          <Lang lnkey={'contributing.code.commits.footer.content'}/>
+        </p>
+        <p>
+          <Lang lnkey={'contributing.code.commits.footer.breaking'}/>
         </p>
         <SyntaxHighlighter language='markdown'>{`
 BREAKING CHANGE: users must now provide a valid JWT token to access
@@ -252,7 +250,12 @@ protected routes.
 
 Closes #123
         `}</SyntaxHighlighter>
-        <p>Here&apos;s a complete example of a correct commit message:</p>
+        <h4>
+          <Lang lnkey={'contributing.code.commits.complete.title'}/>
+        </h4>
+        <p>
+          <Lang lnkey={'contributing.code.commits.complete.description'}/>
+        </p>
         <SyntaxHighlighter language='markdown'>{`
 fix(framework): resolve issue with string injection from env
 
@@ -262,42 +265,39 @@ or untyped parameter, the value would never be injected from the environment.
 Closes #294
         `}</SyntaxHighlighter>
 
-        <h3>Revert commits</h3>
-        <p>If the commit reverts a previous commit, it should begin with
-          <span className="mono-text">revert:</span>, followed by the header
-          of the reverted commit.
+        <h4>
+          <Lang lnkey={'contributing.code.commits.reverts.title'}/>
+        </h4>
+        <p>
+          <Lang lnkey={'contributing.code.commits.reverts.description'}/>
         </p>
-        <p>The content of the commit message body should contain:</p>
+        <p>
+          <Lang lnkey={'contributing.code.commits.reverts.content'}/>
+        </p>
         <ul className="card-list">
-          <li>information about the SHA of the commit being reverted in
-            the following format: <span className="mono-text">This
-            reverts commit &lt;SHA&gt;</span>
+          <li>
+            <Lang lnkey={'contributing.code.commits.reverts.list_item_1'}/>
           </li>
           <li>
-            a clear description of the reason for reverting the commit message.
+            <Lang lnkey={'contributing.code.commits.reverts.list_item_2'}/>
           </li>
         </ul>
       </div>
     </div>
     <div className='card'>
-      <h2>Documentation</h2>
+      <h2>
+        <Lang lnkey={'contributing.documentation.title'}/>
+      </h2>
       <p>
-        Documentation is an underrated part of every software.
-        Adding any kind of clarification, example or improvement is highly
-        appreciated and encouraged. The ones writing the documentation are
-        the unsung heroes of open and closed source software.
+        <Lang lnkey={'contributing.documentation.description'}/>
       </p>
     </div>
     <div className='card'>
-      <h3>Examples</h3>
+      <h3>
+        <Lang lnkey={'contributing.examples.title'}/>
+      </h3>
       <p>
-        For examples please keep in mind, that we don&apos;t
-        want to overload any api. Don&apos;t
-        use high load tasks,
-        don&apos;t
-        use apis that are not for public use. It is expected that
-        examples will be run and they must not damage
-        anyone, including third parties.
+        <Lang lnkey={'contributing.examples.description'}/>
       </p>
     </div>
   </section>}
