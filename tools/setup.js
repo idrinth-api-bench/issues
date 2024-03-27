3d827defed3e1e1b060f493d71d27fdf3e41a511
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-27 18:58:47 +0100

    fix(dockerimages, tools): docker build fix, setup fix

--- tools/setup.js
+++ tools/setup.js
@@ -1,9 +1,9 @@
 import exec from './src/exec.js';
 
-exec('npm install', true,);
-exec('cd framework && npm install', true,);
-exec('cd documentation-website && npm install', true,);
+exec('npm ci', true,);
+exec('cd framework && npm ci', true,);
+exec('cd documentation-website && npm ci', true,);
 exec('cd documentation-website && npm run build', true,);
-exec('cd history-website && npm install', true,);
-exec('cd history-website && npm run build', true,);
-exec('cd history-microservice && npm install', true,);
+exec('cd history-website && npm ci', true,);
+exec('cd history-website && npm ci', true,);
+exec('cd history-microservice && npm ci', true,);
