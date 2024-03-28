0ec7fa8cbc1df041c39f98a14e46ae71e35f0b67
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-28 22:23:15 +0100

    eslint fixes

--- documentation-website/vite.config.ts
+++ documentation-website/vite.config.ts
@@ -2,7 +2,9 @@ import {
   defineConfig,
 } from 'vite';
 import react from '@vitejs/plugin-react';
-import {plugin} from '@idrinth/rollup-plugin-react-modular-css';
+import {
+  plugin,
+} from '@idrinth/rollup-plugin-react-modular-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
