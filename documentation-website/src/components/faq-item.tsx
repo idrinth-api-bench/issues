3d5ffd0e860d5c39ed2a3fd8310d7e8c3c37d42e
Habib Ur Rehman <42462183+Hbrehman@users.noreply.github.com>
2024-03-29 07:28:24 +0500

    refactor(documentation-website): extract FAQ element in its own component (#541)
    fixes #505 
    fixes #523 
    
    ---------
    
    Co-authored-by: Björn Büttner <Idrinth@users.noreply.github.com>

---
+++ documentation-website/src/components/faq-item.tsx
@@ -0,0 +1,65 @@
+import React, {
+  useState,
+} from 'react';
+import {
+  ONE,
+} from '../constants';
+import {
+  Lang,
+} from './lang';
+
+const FaqItem = ({
+  index,
+}: { index: number },) => {
+  const [
+    isOpen,
+    setIsOpen,
+  ] = useState(false,);
+
+  const toggleOpen = () => {
+    setIsOpen(! isOpen,);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>,) => {
+    if (e.key === 'Enter') {
+      toggleOpen();
+    }
+  };
+
+  return (
+    <>
+      <div
+        onClick={() => toggleOpen()}
+        onKeyDown={handleKeyDown}
+        tabIndex={0}
+        role="button"
+      >
+        <div className="accordion-section">
+          <h5>
+            {
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-expect-error
+              <Lang lnkey={`home.faq.questions.title_${ index + ONE }`} />
+            }
+          </h5>
+          {isOpen ?
+            <span className="up-arrow"></span>
+            :
+            <span className="down-arrow"></span>
+          }
+        </div>
+      </div>
+      {isOpen &&
+        <p>
+          {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            <Lang lnkey={`home.faq.questions.description_${ index + ONE }`} />
+          }
+        </p>
+      }
+    </>
+  );
+};
+
+export default FaqItem;
--- documentation-website/src/components/faq.tsx
+++ documentation-website/src/components/faq.tsx
@@ -1,65 +1,11 @@
-import React, {
-  useState,
-} from 'react';
-import {
-  ONE,
-} from '../constants';
-import {
-  Lang,
-} from './lang';
+import React from 'react';
+import FaqItem from './faq-item';
 
-const Faqs = () => {
-
-  //change the length if more FAQ is added to the en.yml.
-  //Add the total no. of questions.
-  const faqLength = 4;
-  const [
-    isOpen,
-    setIsOpen,
-  ] = useState(Array(faqLength,).fill(false,),);
-
-  const toggleOpen = (index: number,) => {
-    const newState = [ ...isOpen, ];
-    newState[index] = ! newState[index];
-    setIsOpen(newState,);
-  };
-
-  return (
-    <div>
-      {[ ...Array(faqLength,), ].map((_, i,) => <div key={`faq${ i }`}>
-        <div onClick={() => toggleOpen(i,)}>
-          <div className='accordion-section'>
-            <div>
-              <h5>
-                {
-                  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-                  // @ts-expect-error
-                  <Lang lnkey={`home.faq.questions.title_${ i+ONE }`} />
-                }
-              </h5>
-            </div>
-            <div>
-              <span>
-                {isOpen[i] ? <i>&#x23F6;</i> : <i>&#x23F7;</i>}
-              </span>
-            </div>
-          </div>
-        </div>
-        <div >
-          {isOpen[i] &&
-          <p>
-            {
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-expect-error
-              <Lang lnkey={`home.faq.questions.description_${ i+ONE }`} />
-            }
-          </p>
-          }
-        </div>
-      </div>,
-      )}
-    </div>
-  );
-};
+const Faqs = () => <div>
+  <FaqItem key={'faq0'} index={0} />
+  <FaqItem key={'faq1'} index={1} />
+  <FaqItem key={'faq2'} index={2} />
+  <FaqItem key={'faq4'} index={3} />
+</div>;
 
 export default Faqs;
