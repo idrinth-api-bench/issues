ec7c4ad01dcc95e340e48fea9e0e11f65f4aa195
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-30 05:22:13 +0100

    feature(history-website): prepare translations refs 279

--- history-website/vite.config.ts
+++ history-website/vite.config.ts
@@ -4,14 +4,10 @@ import {
 import {
   svelte,
 } from '@sveltejs/vite-plugin-svelte';
-import dynamicImportVariables from '@rollup/plugin-dynamic-import-vars';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    dynamicImportVariables({
-      include: '*.svelte',
-    },),
     svelte(),
   ],
 },);
