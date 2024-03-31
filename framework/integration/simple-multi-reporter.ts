aef58937bc778e1c92d9434ead2469fdbca9691e
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-31 16:29:50 +0200

    exclude the internal reporter

--- framework/integration/simple-multi-reporter.ts
+++ framework/integration/simple-multi-reporter.ts
@@ -1,9 +1,22 @@
-import multiReporter from '../src/reporter/multi-reporter.js';
 import csvReporter from '../src/reporter/csv-reporter.js';
 import consoleReporter from '../src/reporter/console-reporter.js';
 import jsonReporter from '../src/reporter/json-reporter.js';
 import htmlReporter from '../src/reporter/html-reporter.js';
+import Reporter from '../src/reporter/reporter';
+import FinishedRun from '../src/finished-run';
 
+interface ReporterList {
+  addReporter: (reporter: Reporter) => void;
+}
+const reporters: Array<Reporter> = [];
+const multiReporter: Reporter&ReporterList = (
+  results: FinishedRun,
+  rootDir: string,
+): void => {
+  for (const reporter of reporters) {
+    reporter(results, rootDir,);
+  }
+};
 multiReporter.addReporter(csvReporter,);
 multiReporter.addReporter(consoleReporter,);
 multiReporter.addReporter(jsonReporter,);
