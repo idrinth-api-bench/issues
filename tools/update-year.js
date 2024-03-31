7f14705371bb247225bb1f916a32b8a233926abe
Björn Büttner <Idrinth@users.noreply.github.com>
2024-03-31 09:00:32 +0200

    pull before push

--- tools/update-year.js
+++ tools/update-year.js
@@ -7,15 +7,18 @@ import exec from './src/exec.js';
 const year = new Date().getFullYear() + 1;
 const previous = new Date().getFullYear();
 
+// eslint-disable-next-line no-template-curly-in-string
+const branch = exec('echo ${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}');
 exec('git config --global user.email "bot@idrinth-api-ben.ch"',);
 exec('git config --global user.name "idrinth api bench bot"',);
 exec(
-  // eslint-disable-next-line no-template-curly-in-string
-  'git checkout -b ${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}',
+  `git checkout -b ${ branch }`,
   true,
 );
+exec('git remote add origin https:://github..com/idrinth/api-bench',);
+exec(`git branch -u origin/${ branch }`, true,);
 exec(
-  'git pull origin/${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}',
+  'git pull',
   true,
 );
 
@@ -24,7 +27,10 @@ for (const file of [
   '/documentation-website/src/components/footer.tsx',
   '/documentation-website/src/pages/license/index.tsx',
 ]) {
-  const content = readFileSync(`${ process.cwd() }${ file }`, 'utf-8',)
+  const content = readFileSync(
+    `${ process.cwd() }${ file }`,
+    'utf-8',
+  )
     .replace(
       new RegExp(`2020-${ previous }`, 'ug',),
       `2020-${ year }`,
@@ -38,7 +44,6 @@ for (const file of [
 exec('git add .',);
 exec(`git commit -m "Update copyright year to ${ year }"`, true,);
 exec(
-  // eslint-disable-next-line no-template-curly-in-string
-  'git push --set-upstream origin ${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}',
+  'git push',
   true,
 );
