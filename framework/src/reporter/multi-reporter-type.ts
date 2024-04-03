128df75eeef38f5b4c78caddc6fe9c33621d4453
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2024-04-03 12:40:24 +0530

    fix(framework): wrong type intersection in multi-reporter (#630)
    Addressed sonar cloud bug , indicating wrong type intersection in  framework/src/reporter/multi-reporter.ts:9
    
    Closes #514

---
+++ framework/src/reporter/multi-reporter-type.ts
@@ -0,0 +1,7 @@
+import ReporterList from './reporter-list.js';
+import Reporter from './reporter.js';
+
+interface MultiReporterType extends ReporterList, Reporter {
+}
+
+export default MultiReporterType;
