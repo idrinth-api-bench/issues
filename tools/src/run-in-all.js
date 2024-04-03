460aee452d194167e1d5ecb883edd7f6f202868a
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-03 10:57:12 +0200

    typos

--- tools/src/run-in-all.js
+++ tools/src/run-in-all.js
@@ -1,6 +1,6 @@
 import exec from './src/exec.js';
 
-export default (command) => {
+export default (command,) => {
   exec(command, true,);
   exec(`cd framework && ${ command }`, true,);
   exec(`cd documentation-website && ${ command }`, true,);
@@ -8,4 +8,4 @@ export default (command) => {
   exec(`cd history-website && ${ command }`, true,);
   exec(`cd history-microservice && ${ command }`, true,);
   exec(`cd mindmap && ${ command }`, true,);
-}
+};
