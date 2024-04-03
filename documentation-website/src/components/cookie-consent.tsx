8d2444bedaf9ba8862d2611e71eba1b863c60a27
Kiril <vtkkllx2367@gmail.com>
2024-04-03 17:22:54 +0300

    cookie-consent extract the locig into different return

--- documentation-website/src/components/cookie-consent.tsx
+++ documentation-website/src/components/cookie-consent.tsx
@@ -14,45 +14,42 @@ const CookieConsent = () => {
 
   const handleConsent = (accept: boolean,) => {
     const action = accept ? 'rememberConsentGiven' : 'forgetConsentGiven';
-    { /*@ts-expect-error _paq can be null*/ }
+    //@ts-expect-error _paq can be null
     window?._paq.push([ action, ],);
     localStorage.setItem('consent-was-asked', 'yes',);
     setConsentWasClosed(true,);
   };
 
-  return (
-    <>
-      {! consentWasClosed &&
-      //@ts-expect-error _paq can be null
-      (! localStorage.getItem('consent-was-asked',) && window?._paq)
-      &&
-        <div className={'cookie-consent'}>
-          <div className={'cookie-consent-description'}>
-            <h2 className={'cookie-title'}>
-              <Lang lnkey={'cookie-consent.title'} />
-            </h2>
-            <p className={'cookie-description'}>
-              <Lang lnkey={'cookie-consent.description'} />
-            </p>
-          </div>
-          <div className={'cookie-consent-buttons'}>
-            <button
-              onClick={() => handleConsent(true,)}
-              className={'cookie-consent-accept-button'}
-            >
-              <Lang lnkey={'cookie-consent.accept'} />
-            </button>
-            <button
-              onClick={() => handleConsent(false,)}
-              className={'cookie-consent-decline-button'}
-            >
-              <Lang lnkey={'cookie-consent.decline'} />
-            </button>
-          </div>
-        </div>
-      }
-    </>
-  );
+  const visible = ! consentWasClosed &&
+  //@ts-expect-error _paq can be null
+  (! localStorage.getItem('consent-was-asked',) && window?._paq);
+
+  return visible ?
+    <div className={'cookie-consent'}>
+      <div className={'cookie-consent-description'}>
+        <h2 className={'cookie-title'}>
+          <Lang lnkey={'cookie-consent.title'} />
+        </h2>
+        <p className={'cookie-description'}>
+          <Lang lnkey={'cookie-consent.description'} />
+        </p>
+      </div>
+      <div className={'cookie-consent-buttons'}>
+        <button
+          onClick={() => handleConsent(true,)}
+          className={'cookie-consent-accept-button'}
+        >
+          <Lang lnkey={'cookie-consent.accept'} />
+        </button>
+        <button
+          onClick={() => handleConsent(false,)}
+          className={'cookie-consent-decline-button'}
+        >
+          <Lang lnkey={'cookie-consent.decline'} />
+        </button>
+      </div>
+    </div>
+    : null;
 };
 
 export default CookieConsent;
