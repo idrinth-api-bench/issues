88ff6bbcf382d999d544e11dd89e44e210da28d7
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-04 07:35:28 +0200

    feature(documentation-website): make syntax highlighter even lighter

--- documentation-website/src/components/code.tsx
+++ documentation-website/src/components/code.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import {
-  LightAsync as SyntaxHighlighter,
+  Light as SyntaxHighlighter,
 } from 'react-syntax-highlighter';
 import './code.css';
+import bash from 'react-syntax-highlighter/dist/esm/languages/hljs/bash';
+// eslint-disable-next-line max-len
+import typescript from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
+// eslint-disable-next-line max-len
+import markdown from 'react-syntax-highlighter/dist/esm/languages/hljs/markdown';
+
+SyntaxHighlighter.registerLanguage('javascript', typescript,);
+SyntaxHighlighter.registerLanguage('typescript', typescript,);
+SyntaxHighlighter.registerLanguage('bash', bash,);
+SyntaxHighlighter.registerLanguage('markdown', markdown,);
+SyntaxHighlighter.registerLanguage('json', typescript,);
 
 interface CodeType {
-  language: string,
+  language: 'bash'|'typescript'|'javascript'|'markdown'|'json',
   children: string|string[],
 }
 
