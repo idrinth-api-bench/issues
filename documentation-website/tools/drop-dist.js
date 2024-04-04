38e13ef8860a4caaf2f262f340762ee4d108d98a
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-04 09:38:10 +0200

    remove old dist before build

---
+++ documentation-website/tools/drop-dist.js
@@ -0,0 +1,23 @@
+import {
+  unlinkSync,
+  readdirSync,
+  rmdirSync,
+  statSync, existsSync
+} from 'fs';
+
+const del = (path,) => {
+  if (! existsSync(path,)) {
+    return;
+  }
+  const stats = statSync(path,);
+  if (stats.isDirectory()) {
+    for (const file of readdirSync(path,)) {
+      del(`${ path }/${ file }`,);
+    }
+    rmdirSync(path,);
+    return;
+  }
+  unlinkSync(path,);
+};
+
+del(process.cwd() + '/dist',);
