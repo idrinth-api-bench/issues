dfc3311401aa166c1a4055e10712efc2b69618d6
owais34 <owaishunter78@gmail.com>
2024-04-05 23:07:40 +0530

    feature(framework): added success & failure check middleware
    Added success & failure check middleware and a standard response type , include error messages in language config

--- framework/src/middlewares/failure-check.ts
+++ framework/src/middlewares/failure-check.ts
@@ -4,10 +4,11 @@ import Result from '../result.js';
 import language from '../helper/language.js';
 import StandardResponse from '../standard-response.js';
 
-const SUCCESS = 'success';
+const FAIL = 'fail';
+const ERROR = 'error';
 
 @staticImplements<Middleware>()
-export default class SuccessCheck {
+export default class FailureCheck {
   public static prepare(request: Request,): Request {
     return request;
   }
@@ -19,36 +20,16 @@ export default class SuccessCheck {
     } catch (e) {
       throw Error(language('invalid_json_body', `${ e }`,),);
     }
-
-    if (response.status !== SUCCESS) {
+    if (! (response.status === FAIL || response.status === ERROR)) {
       throw new Error(
-        language('response_not_success', 'status', `${ response.status }`,),
+        language('response_not_failure', 'status', `${ response.status }`,),
       );
     }
 
-    if (response.success !== true) {
+    if (response.success !== false) {
       throw new Error(
-        language('response_not_success', 'success', `${ response.success }`,),
+        language('response_not_failure', 'success', `${ response.success }`,),
       );
     }
   }
 }
-
-/*
-Below Document Defines the standards upon which most apis will be constructed
-
-1) JSEND - https://github.com/omniti-labs/jsend
-{
-  "status": "success/fail/error",
-  "data":{},
-  "message":"Present only when status is error"
-}
-
-2) Another Common Format
-{
-  "success": false/true,
-  "message": "Used mostly in case of error message,
-  "error_code": 1308,
-  "data": {}
-}
-*/
---
+++ framework/src/standard-response.ts
@@ -0,0 +1,8 @@
+export interface StandardResponse {
+  status?: string;
+
+  success?: boolean;
+}
+
+export default StandardResponse;
+
