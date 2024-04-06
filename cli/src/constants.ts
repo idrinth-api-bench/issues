f83468b053b0f683cf22aeb0aebebf79f25f1520
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-06 13:32:15 +0200

    feature(global): introduce knip (#689)
    fixes #685

--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -1,7 +1,5 @@
 export const EMPTY = 0;
 export const FIRST_ARGUMENT = 2;
 export const DEFAULT_LANGUAGE = 'en';
-export const FIRST = 0;
 export const INDENTATION_SPACES = 2;
-export const ONE = 1;
 export const NEXT = 1;
