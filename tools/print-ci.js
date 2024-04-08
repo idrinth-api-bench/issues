ce16d9e316efcf7557a25c1cf12209ee1d03ce22
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-08 10:05:42 +0200

    refactor(tools): add print-ci
    fixes #0

--- tools/print-ci.js
+++ tools/print-ci.js
@@ -5,6 +5,10 @@ import {
 import {
   readdirSync, readFileSync,
 } from 'fs';
+import {
+  EMPTY,
+  ARRAY_LENGTH_OFFSET,
+} from './src/constants.js';
 
 const jobs = {};
 for (const file of readdirSync(process.cwd() + '/.github/workflows',)) {
@@ -13,6 +17,7 @@ for (const file of readdirSync(process.cwd() + '/.github/workflows',)) {
     name,
   ] = file.split('.',);
   if (name === 'codeql') {
+    // eslint-disable-next-line no-continue
     continue;
   }
   jobs[type] = jobs[type] ?? {};
@@ -23,37 +28,46 @@ for (const file of readdirSync(process.cwd() + '/.github/workflows',)) {
   for (const key of Object.keys(data.jobs,)) {
     const runs = [];
     for (const step of data.jobs[key].steps) {
-      if (step.run && step.run.match(/npm run/u,) && ! step.run.endsWith('|| true',)) {
+      if (
+        step.run
+        && step.run.match(/npm run/u,)
+        && ! step.run.match(/npm ci/u,)
+        && ! step.run.endsWith('|| true',)
+      ) {
         runs.push(step.run.replace(/^ *| *$/ug, '',),);
       }
     }
     if (runs.length === 0) {
+      // eslint-disable-next-line no-continue
       continue;
     }
-    if (data.jobs[key].strategy && data.jobs[key].strategy.matrix) {
-      let combinations = [ '', ];
-      for (const skey of Object.keys(data.jobs[key].strategy.matrix,)) {
-        const newCombinations = [];
-        for (const val of data.jobs[key].strategy.matrix[skey]) {
-          for (const known of combinations) {
-            newCombinations.push(
-              `${ known }|${ skey }=${ val }`
-                .replace(/^\|/u, '',),
-            );
-          }
-        }
-        combinations = newCombinations;
-      }
-      for (const combination of combinations) {
-        jobs[type][`${ name }|${ combination }`] = {
-          on: jobs[type][name].on,
-        };
-        jobs[type][`${ name }|${ combination }`].jobs = jobs[type][`${ name }|${ combination }`].jobs || {};
-        jobs[type][`${ name }|${ combination }`].jobs[key] = runs[runs.length - 1];
-      }
-    } else {
+    if (! data.jobs[key].strategy || ! data.jobs[key].strategy.matrix) {
       jobs[type][name].jobs = jobs[type][name].jobs || {};
-      jobs[type][name].jobs[key] = runs[runs.length - 1];
+      jobs[type][name].jobs[key] = runs[runs.length - ARRAY_LENGTH_OFFSET];
+      continue;
+    }
+    let combinations = [ '', ];
+    for (const skey of Object.keys(data.jobs[key].strategy.matrix,)) {
+      const newCombinations = [];
+      // eslint-disable-next-line max-depth
+      for (const val of data.jobs[key].strategy.matrix[skey]) {
+        // eslint-disable-next-line max-depth
+        for (const known of combinations) {
+          newCombinations.push(
+            `${ known }|${ skey }=${ val }`
+              .replace(/^\|/u, '',),
+          );
+        }
+      }
+      combinations = newCombinations;
+    }
+    for (const combination of combinations) {
+      jobs[type][`${ name }|${ combination }`] = {
+        on: jobs[type][name].on,
+      };
+      const nm = `${ name }|${ combination }`;
+      jobs[type][nm].jobs = jobs[type][nm].jobs || {};
+      jobs[type][nm].jobs[key] = runs[runs.length - ARRAY_LENGTH_OFFSET];
     }
   }
 }
@@ -63,7 +77,7 @@ for (const type of Object.keys(jobs,)) {
       delete jobs[type][job];
     }
   }
-  if (Object.keys(jobs[type],).length === 0) {
+  if (Object.keys(jobs[type],).length === EMPTY) {
     delete jobs[type];
   }
 }
