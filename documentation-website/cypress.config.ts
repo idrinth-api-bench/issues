efceee4395b920d710f3dd08bd16475a8840d197
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-08 06:14:06 +0200

    feature(examples): run in parallel
    fixes #731

--- documentation-website/cypress.config.ts
+++ documentation-website/cypress.config.ts
@@ -3,10 +3,12 @@ import {
 } from 'cypress';
 import task from '@cypress/code-coverage/task';
 import useBabelRC from '@cypress/code-coverage/use-babelrc';
+import paralell from 'cypress-split';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config,) {
+      paralell(on, config,);
       task(on, config,);
       on('file:preprocessor', useBabelRC,);
       return config;
