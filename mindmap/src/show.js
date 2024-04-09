5363ce7e54b77959daf4162214b764751d9939a6
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-09 03:31:37 +0200

    feature(mindmap): add linkedin survey results
    fixes #746

--- mindmap/src/show.js
+++ mindmap/src/show.js
@@ -5,7 +5,20 @@ function show(element,) {
   }
   const img = document.createElement('img',);
   img.setAttribute('src', element.getAttribute('data-image',),);
-  img.setAttribute('class', 'modal',);
   img.onmouseleave = () => img.parentElement.removeChild(img,);
+  if (element.hasAttribute('title',)) {
+    img.setAttribute('title', element.getAttribute('title',),);
+  }
+  if (element.hasAttribute('href',)) {
+    const a = document.createElement('a',);
+    a.setAttribute('href', element.getAttribute('href',));
+    a.setAttribute('target', '_blank',);
+    a.setAttribute('rel', 'noreferrer',);
+    a.setAttribute('class', 'modal',);
+    a.appendChild(img,);
+    document.body.appendChild(a,);
+    return;
+  }
+  img.setAttribute('class', 'modal',);
   document.body.append(img,);
 }
