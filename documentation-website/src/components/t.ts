8d263e21b3d99c4ca8273c9e7ea7b54c7d38c844
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-11 08:13:02 +0200

    bug(documentation-website,framework,cli,history-website): __proto__ i… (#778)
    fixes #772

--- documentation-website/src/components/t.ts
+++ documentation-website/src/components/t.ts
@@ -8,6 +8,9 @@ import files from '../locales/files.ts';
 
 // eslint-disable-next-line complexity
 export default async(lnkey: languageKey, global?: object,): Promise<string> => {
+  if (lnkey.match(/(^|\.)(__proto__|valueOf|toString)(\.|$)/u,)) {
+    return '';
+  }
   const that: Window = (global ?? window) as Window;
   const language = (that?.Navigator?.language ?? 'en')
     .replace(/-.*$/u, '',);
