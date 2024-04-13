e6091d7b998215b1c5bc1f5a43694ab13e11c8d3
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-13 22:16:57 +0200

    Update framework/src/storage/postgres-storage.ts
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- framework/src/storage/postgres-storage.ts
+++ framework/src/storage/postgres-storage.ts
@@ -22,13 +22,16 @@ export class PostgresStorage implements Storage {
     user: string = 'idrinth-api-bench',
     database: string = 'idrinth-api-bench',
   ) {
-    this.connection = postgres({
-      host,
-      user,
-      password,
-      database,
-      port,
-    },);
+this.connection = postgres({
+  host,
+  user,
+  password,
+  database,
+  port,
+  max: 10, // maximum size of the pool
+  idleTimeoutMillis: 30000, // close idle clients after 30 seconds
+  connectionTimeoutMillis: 2000, // return an error after 2 seconds if connection could not be established
+});
     this.connection.unsafe(
       // eslint-disable-next-line max-len
       `CREATE TABLE IF NOT EXISTS \`${ project }\` (aid BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,id varchar(255) NOT NULL,\`day\` Date NOT NULL,errors int NOT NULL,\`count\` int NOT NULL,stdv100 BIGINT NOT NULL,stdv80 BIGINT NOT NULL,min80 BIGINT NOT NULL,min100 BIGINT NOT NULL,max80 BIGINT NOT NULL,max100 BIGINT NOT NULL,avg80 DECIMAL NOT NULL,avg100 DECIMAL NOT NULL,median80 DECIMAL NOT NULL,median100 DECIMAL NOT NULL,msgs TEXT)`,
