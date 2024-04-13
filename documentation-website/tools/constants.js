28fc3f9dfc11a0ceaadca9eb1492d2daad0a777d
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-13 18:01:44 +0200

    fix runner
    fixes #0

--- documentation-website/tools/constants.js
+++ documentation-website/tools/constants.js
@@ -1,3 +1,2 @@
 export const EXIT_FAILURE = 1;
-export const EXIT_SUCCESS = 0;
-export const FIRST_MATCH = 0;
+
--- documentation-website/tools/lighthouse-report.js
+++ documentation-website/tools/lighthouse-report.js
@@ -1,11 +1,8 @@
 import lighthouse from 'lighthouse';
-import * as chromeLauncher from 'chrome-launcher';
-import jp from 'jsonpath';
+import chromeLauncher from 'chrome-launcher';
 import process from 'process';
 import {
   EXIT_FAILURE,
-  EXIT_SUCCESS,
-  FIRST_MATCH,
 } from './constants.js';
 
 const chrome = await chromeLauncher.launch({
@@ -18,8 +15,6 @@ const options = {
   port: chrome.port,
 };
 
-let EXIT_CODE = EXIT_SUCCESS;
-
 try {
   // eslint-disable-next-line max-len
   const liveSiteReport = await lighthouse('https://idrinth-api-ben.ch/', options,);
@@ -29,39 +24,18 @@ try {
   const dev_report = JSON.parse(devSiteReport.report,);
 
   const metricFields = [
-    {
-      jsonpath: '$.categories.performance.score',
-      name: 'performance',
-    },
-    {
-      jsonpath: '$.categories.accessibility.score',
-      name: 'accessibility',
-    },
-    {
-      jsonpath: '$.categories.best-practices.score',
-      name: 'best practices',
-    },
-    {
-      jsonpath: '$.categories.seo.score',
-      name: 'seo',
-    },
-    {
-      jsonpath: '$.categories.pwa.score',
-      name: 'progressive web app',
-    },
+    'performance',
+    'accessibility',
+    'best-practices',
+    'seo',
+    'pwa',
   ];
 
   for (const metricField of metricFields) {
-    const devMetricScore = jp.query(
-      dev_report,
-      metricField.jsonpath,
-    )[FIRST_MATCH];
-    const prodMetricScore = jp.query(
-      prod_report,
-      metricField.jsonpath,
-    )[FIRST_MATCH];
+    const devMetricScore = dev_report?.categories[metricField]?.score;
+    const prodMetricScore = prod_report?.categories[metricField]?.score;
 
-    if (devMetricScore === null) {
+    if (! devMetricScore) {
       throw new Error(
         // eslint-disable-next-line max-len
         `Failure in fetching metric score for current site, found ${ metricField.name } to be null`,
@@ -74,13 +48,11 @@ try {
         // eslint-disable-next-line max-len
         `${ metricField.name } score reduced to ${ devMetricScore } from ${ prodMetricScore }`,
       );
-      EXIT_CODE = EXIT_FAILURE;
+      process.exitCode = EXIT_FAILURE;
     }
   }
 } catch (err) {
   // eslint-disable-next-line no-console
   console.error(err,);
-  EXIT_CODE = EXIT_FAILURE;
+  process.exitCode = EXIT_FAILURE;
 }
-
-process.exit(EXIT_CODE,);
