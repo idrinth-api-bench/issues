fecfe526b76d4fd87179e1d2d30332831f584e18
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-16 16:15:40 +0200

    bug(ci): manually set values for fair comparison (#824)

--- documentation-website/tools/lighthouse-report.js
+++ documentation-website/tools/lighthouse-report.js
@@ -10,51 +10,45 @@ const chrome = await chromeLauncher.launch({
 },);
 
 const options = {
-  logLevel: 'info',
+  logLevel: 'error',
   output: 'json',
   port: chrome.port,
 };
 
-try {
-  // eslint-disable-next-line max-len
-  const liveSiteReport = await lighthouse('https://idrinth-api-ben.ch/', options,);
-  const prod_report = JSON.parse(liveSiteReport.report,);
+const devSiteReport = await lighthouse('http://localhost:8080/', options,);
+const dev_report = JSON.parse(devSiteReport.report,);
 
-  const devSiteReport = await lighthouse('http://localhost:8080/', options,);
-  const dev_report = JSON.parse(devSiteReport.report,);
+const metricFields = {
+  performance: 0.55,
+  accessibility: 0.85,
+  'best-practices': 1,
+  seo: 1,
+  pwa: 0.35,
+};
 
-  const metricFields = [
-    'performance',
-    'accessibility',
-    'best-practices',
-    'seo',
-    'pwa',
-  ];
+for (const metricField of Object.keys(metricFields,)) {
+  const devMetricScore = dev_report?.categories[metricField]?.score;
 
-  for (const metricField of metricFields) {
-    const devMetricScore = dev_report?.categories[metricField]?.score;
-    const prodMetricScore = prod_report?.categories[metricField]?.score;
-
-    if (! devMetricScore) {
-      throw new Error(
-        // eslint-disable-next-line max-len
-        `Failure in fetching metric score for current site, found ${ metricField } to be null`,
-      );
-    }
-
-    if (devMetricScore < prodMetricScore) {
-      // eslint-disable-next-line no-console
-      console.error(
-        // eslint-disable-next-line max-len
-        `${ metricField } score reduced to ${ devMetricScore } from ${ prodMetricScore }`,
-      );
-      process.exitCode = EXIT_FAILURE;
-    }
+  if (! devMetricScore) {
+    // eslint-disable-next-line no-console
+    console.error(
+      // eslint-disable-next-line max-len
+      `Failure in fetching metric score for current site, found ${ metricField } to be null`,
+    );
+    process.exitCode = EXIT_FAILURE;
+  } else if (devMetricScore < metricFields[metricField]) {
+    // eslint-disable-next-line no-console
+    console.error(
+      // eslint-disable-next-line max-len
+      `${ metricField } score reduced to ${ devMetricScore } from ${ metricFields[metricField] }`,
+    );
+    process.exitCode = EXIT_FAILURE;
+  } else {
+    // eslint-disable-next-line no-console
+    console.error(
+      // eslint-disable-next-line max-len
+      `${ metricField } score is at ${ devMetricScore }`,
+    );
   }
-} catch (err) {
-  // eslint-disable-next-line no-console
-  console.error(err,);
-  process.exitCode = EXIT_FAILURE;
 }
-
 process.exit();
