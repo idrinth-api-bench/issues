77b0f2f48f19c0e623dcc67754d2c9f6e8cb984f
Björn Büttner <Idrinth@users.noreply.github.com>
2024-04-16 23:19:17 +0200

    feature(framework): the config is now build from both command line options and env
    fixes #0

--- framework/src/cli/config/to-value.ts
+++ framework/src/cli/config/to-value.ts
@@ -2,8 +2,10 @@ import {
   BASE_10_RADIX,
 } from '../../constants.js';
 
+const IS_NUMERIC = /^\d+$/u;
+
 export default (value: string,) => {
-  if (value.match(/^\d+$/u,)) {
+  if (IS_NUMERIC.test(value,)) {
     return Number.parseInt(value, BASE_10_RADIX,);
   }
   return value;
