1b763b5a570a5e098cb1cfd5ac4e5632c93d44e6
Owais Hasnath Ahmed <37238759+owais34@users.noreply.github.com>
2024-04-23 21:32:07 +0530

    feature(cli): add a curl cli route builder (#855)
    Added a binary that takes a curl like cli command and converts it into a route file
    Command format : generate-benchmark-from-curl
    -X POST // curl request method GET/POST/PUT
    -H 'Content-Type: application/json' // custom request headers
    -d '{\"name\":\"Joe Biden\"}' // request body to be sent
    url // request url
    
    Closes #813
    
    ---------
    
    Co-authored-by: Björn Büttner <Idrinth@users.noreply.github.com>

--- cli/src/constants.ts
+++ cli/src/constants.ts
@@ -3,3 +3,6 @@ export const FIRST_ARGUMENT = 2;
 export const DEFAULT_LANGUAGE = 'en';
 export const INDENTATION_SPACES = 2;
 export const NEXT = 1;
+export const KEY_INDEX = 0;
+export const VALUE_INDEX = 0;
+export const SPLIT_LIMIT = 1;
