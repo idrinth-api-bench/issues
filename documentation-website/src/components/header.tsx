9b02d4be4d494397b9155f64084c7b2827607d46
Marika Mella <marika.developer@gmail.com>
2024-04-28 11:58:34 +0200

    refactor(documentation-website): extract header styles (#534)
    Create header.scss file to contain the styles extracted from index.scss

--- documentation-website/src/components/header.tsx
+++ documentation-website/src/components/header.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import IAB from '../assets/iab.svg';
+import {
+  MdArrowDropDown,
+} from 'react-icons/md';
 import {
   NavLink,
 } from 'react-router-dom';
 import pkg from '../../package.json' with {
   type: 'json'
 };
-import Lang from './lang.tsx';
+import IAB from '../assets/iab.svg';
 import DarkModeButton from './dark-mode-button.tsx';
+import './header.scss';
+import Lang from './lang.tsx';
 import SocialLink from './social-link.tsx';
-import {
-  MdArrowDropDown,
-} from 'react-icons/md';
 
 const preventOpenLink = (event: React.MouseEvent<SVGElement, MouseEvent>,) => {
   event.preventDefault();
@@ -28,7 +29,7 @@ const preventOpenLink = (event: React.MouseEvent<SVGElement, MouseEvent>,) => {
 
 const Header = ({
   window,
-}: {window: Window},) => <header>
+}: {window: Window},) => <header className={'header'}>
   <div>
     <NavLink id="logo" to="/">
       <img src={IAB} alt="@idrinth/api-bench" />
