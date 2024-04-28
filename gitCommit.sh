3522b0096f32967e8649c8cff43817f13314866e
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-04-27 23:31:32 -0700

    build(documentation-website): refined gitCommit.sh \n added more automation to gitCommit.sh \n closes #913

--- gitCommit.sh
+++ gitCommit.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Commit changes form for easy commits
 
+read -rp "Enter your working branch name: " branch
 read -rp "Enter your task type: " task
 read -rp "Enter the task scope: " scope
 read -rp "Enter the task summary: " summary
@@ -9,4 +10,12 @@ read -rp "What issue number does this address? " issue
 
 git add .
 git commit -m "$task($scope): $summary \n $description \n closes #$issue"
+git pull
 git push
+
+git checkout the-one
+git merge $branch
+git push
+
+git checkout $branch
+echo 'Your are now on your working branch. Happy coding!'
