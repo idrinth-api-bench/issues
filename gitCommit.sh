e44ea072d531a48ea033b8284d21d24aeb485db2
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2024-04-27 17:24:36 -0700

    build(framework): easy commit script \n added a bash script to walk users though commits \n closes #913

---
+++ gitCommit.sh
@@ -0,0 +1,11 @@
+#! Commit changes form for easy commits
+
+read -rp "Enter your task type: " task
+read -rp "Enter the task scope: " scope
+read -rp "Enter the task summary: " summary
+read -rp "Enter the task description: " description
+read -rp "What issue number does this address? " issue
+
+git add .
+git commit -m "$task($scope): $summary \n $description \n closes #$issue"
+git push
