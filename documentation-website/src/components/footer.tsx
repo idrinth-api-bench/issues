26516d4d4e82f32ae4d8b9f641f250849c3d2618
Marika Mella <116084393+marikadeveloper@users.noreply.github.com>
2024-04-29 12:05:06 +0200

    Issue 890 language switch test (#917)
    fixes #890

--- documentation-website/src/components/footer.tsx
+++ documentation-website/src/components/footer.tsx
@@ -1,41 +1,42 @@
 import React from 'react';
-import Lang from './lang.tsx';
 import {
   NavLink,
 } from 'react-router-dom';
 import CookieConsent from './cookie-consent.tsx';
+import Lang from './lang.tsx';
 import LanguageSwitch from './language-switch.tsx';
 
-const Footer = () => <footer>
+const Footer = ({
+  window,
+}: { window: Window },) => <footer>
   <span>©2020-2024 Björn Büttner and contributors.</span>
   <ul>
     <li>
-      <LanguageSwitch />
+      <LanguageSwitch window={window || {}} />
     </li>
     <li>
       <CookieConsent />
     </li>
     <li>
-      <NavLink to="/license/">
-        <Lang lnkey='nav.license'/>
+      <NavLink to='/license/'>
+        <Lang lnkey='nav.license' />
       </NavLink>
     </li>
     <li>
-      <NavLink to="/imprint/">
-        <Lang lnkey='nav.imprint'/>
+      <NavLink to='/imprint/'>
+        <Lang lnkey='nav.imprint' />
       </NavLink>
     </li>
     <li>
-      <NavLink to="/support/">
-        <Lang lnkey='nav.support'/>
+      <NavLink to='/support/'>
+        <Lang lnkey='nav.support' />
       </NavLink>
     </li>
     <li>
-      <NavLink to="/code-of-conduct/">
-        <Lang lnkey='nav.code-of-conduct'/>
+      <NavLink to='/code-of-conduct/'>
+        <Lang lnkey='nav.code-of-conduct' />
       </NavLink>
     </li>
   </ul>
 </footer>;
-
 export default Footer;
--- documentation-website/test/components/footer.ts
+++ documentation-website/test/components/footer.ts
@@ -1,14 +1,17 @@
-import Footer from '../../src/components/footer.tsx';
 import {
   expect,
 } from 'chai';
+import Window from 'window';
+import Footer from '../../src/components/footer.tsx';
+
+const window = new Window();
 
 describe('components/footer', () => {
   it('should be a function', () => {
     expect(Footer,).to.be.a('function',);
   },);
   it('() should be an object', () => {
-    const result = Footer();
+    const result = Footer(window,);
     expect(result,).to.be.a('object',);
   },);
 },);
