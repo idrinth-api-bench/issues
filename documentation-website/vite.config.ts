f205292c0243489e9fe529e1699b460a030c9cd4
Björn Büttner <Idrinth@users.noreply.github.com>
2024-05-01 20:37:35 +0200

    feature(documentation-website): add million
    fixes #645

--- documentation-website/vite.config.ts
+++ documentation-website/vite.config.ts
@@ -2,11 +2,12 @@ import {
   defineConfig,
 } from 'vite';
 import react from '@vitejs/plugin-react';
+import attributes from '@babel/plugin-syntax-import-attributes';
 import plugin from '@idrinth/rollup-plugin-react-modular-css';
 import istanbul from 'babel-plugin-istanbul';
 import million from 'million/compiler';
 
-const babelPlugins = [];
+const babelPlugins = [ attributes, ];
 if (process.env.LIVE_SITE !== 'true') {
   babelPlugins.push(istanbul,);
 }
@@ -14,7 +15,18 @@ if (process.env.LIVE_SITE !== 'true') {
 export default defineConfig({
   build: {
     rollupOptions: {
-      plugins: [ plugin(), ],
+      output: {
+        importAttributesKey: 'with',
+      },
+      plugins: [
+        {
+          name: '@idrinth/vite-plugin-import-json',
+          transform(code, id,) {
+            if (! id.startsWith('node_modules',) && code.match(/json/ug,)) {
+              console.log(id);
+            }
+          },
+        }, plugin(), ],
     },
   },
   plugins: [
