f30a7e4a39af6ce0b008aa8bdadf91e1073f86ea
Björn Büttner <Idrinth@users.noreply.github.com>
2024-05-06 01:41:22 +0200

    Update middlewares.cy.ts

--- documentation-website/cypress/e2e/middlewares.cy.ts
+++ documentation-website/cypress/e2e/middlewares.cy.ts
@@ -11,8 +11,7 @@ describe('usage/middlewares', () => {
     cy.url().should('eq', BASE_URL + page.middlewares,);
   },);
   it('title matches', () => {
-    cy.title().should('eq', 'Middlewares | Usage | ' +
-    '@idrinth/api-bench',);
+    cy.title().should('eq', 'Middlewares | Usage | @idrinth/api-bench',);
   },);
   it('h1 matches', () => {
     cy.get('h1',)
